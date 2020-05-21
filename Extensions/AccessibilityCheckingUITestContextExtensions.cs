88010804bd057474bfa114000cfd09764c1ecffb
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-05-21 15:25:20 +0200

    Accessibility checking methods now can also be configured on each method call

--- Extensions/AccessibilityCheckingUITestContextExtensions.cs
+++ Extensions/AccessibilityCheckingUITestContextExtensions.cs
@@ -7,13 +7,35 @@ namespace Lombiq.Tests.UI.Extensions
 {
     public static class AccessibilityCheckingUITestContextExtensions
     {
-        public static void AssertAccessibility(this UITestContext context)
+        /// <summary>
+        /// Executes assertions on the result of an axe accessibility analysis. Note that you need to run this after
+        /// every page load, it won't accumulate during a session.
+        /// </summary>
+        /// <param name="assertAxeResult">
+        /// The assertion logic to run on the result of an axe accessibility analysis. If <c>null</c> then the 
+        /// assertion supplied in the context will be used.
+        /// </param>
+        /// <param name="axeBuilderConfigurator">
+        /// A delegate to configure the <see cref="AxeBuilder"/> instance. Will be applied after the configurator 
+        /// supplied in the context.
+        /// </param>
+        public static void AssertAccessibility(
+            this UITestContext context,
+            Action<AxeResult> assertAxeResult = null,
+            Action<AxeBuilder> axeBuilderConfigurator = null)
         {
-            var axeResult = context.AnalyzeAccessibility();
+            var axeResult = context.AnalyzeAccessibility(axeBuilderConfigurator);
 
             try
             {
-                context.Configuration.AccessibilityCheckingConfiguration.AssertAxeResult?.Invoke(axeResult);
+                if (assertAxeResult == null)
+                {
+                    context.Configuration.AccessibilityCheckingConfiguration.AssertAxeResult?.Invoke(axeResult);
+                }
+                else
+                {
+                    assertAxeResult?.Invoke(axeResult);
+                }
             }
             catch (Exception ex)
             {
@@ -21,10 +43,20 @@ namespace Lombiq.Tests.UI.Extensions
             }
         }
 
-        public static AxeResult AnalyzeAccessibility(this UITestContext context)
+        /// <summary>
+        /// Runs an axe accessibility analysis. Note that you need to run this after every page load, it won't 
+        /// accumulate during a session.
+        /// </summary>
+        /// <param name="axeBuilderConfigurator">
+        /// A delegate to configure the <see cref="AxeBuilder"/> instance. Will be applied after the configurator 
+        /// supplied in the context.
+        /// </param>
+        /// <returns></returns>
+        public static AxeResult AnalyzeAccessibility(this UITestContext context, Action<AxeBuilder> axeBuilderConfigurator = null)
         {
             var axeBuilder = new AxeBuilder(context.Scope.Driver);
             context.Configuration.AccessibilityCheckingConfiguration.AxeBuilderConfigurator?.Invoke(axeBuilder);
+            axeBuilderConfigurator?.Invoke(axeBuilder);
             return axeBuilder.Analyze();
         }
     }
