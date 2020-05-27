5d5e78573becfef4de1531ec056f4a3d68df8c42
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-05-27 23:25:08 +0200

    Using WebDriverManager to automate driver setup

--- Docs/Tools.md
+++ Docs/Tools.md
@@ -7,6 +7,7 @@
   - For Edge only the new Chromium-based Edge is supported that you can download from [here](https://www.microsoft.com/en-us/edge).
   - For Firefox it needs to be installed too.
   - For IE you'll need to do [some manual configuration](https://github.com/SeleniumHQ/selenium/wiki/InternetExplorerDriver#required-configuration) in the browser.
+- Browser driver setup is automated with [WebDriverManager.NET](https://github.com/rosolko/WebDriverManager.Net).
 - There are multiple recording tools available for Selenium but the "official" one which works pretty well is [Selenium IDE](https://www.selenium.dev/selenium-ide/) (which is a Chrome/Firefox extension). To fine-tune XPath queries and CSS selectors and also to record tests check out [ChroPath](https://chrome.google.com/webstore/detail/chropath/ljngjbnaijcbncmcnjfhigebomdlkcjo/).
 - Recorded tests are then executed from xUnit tests.
 - Accessibility checking can be done with [axe](https://github.com/dequelabs/axe-core) via [Selenium.Axe for .NET](https://github.com/TroyWalshProf/SeleniumAxeDotnet).
--- Services/WebDriverFactory.cs
+++ Services/WebDriverFactory.cs
@@ -5,13 +5,20 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Remote;
 using System;
+using System.Collections.Concurrent;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace Lombiq.Tests.UI.Services
 {
     public static class WebDriverFactory
     {
+        private readonly static ConcurrentDictionary<string, Lazy<bool>> _driverSetups = new ConcurrentDictionary<string, Lazy<bool>>();
+
+
         public static ChromeDriver CreateChromeDriver(TimeSpan pageLoadTimeout) =>
-            WrapIntoException(() =>
+            CreateDriver(() =>
             {
                 var options = new ChromeOptions().SetCommonOptions();
 
@@ -21,27 +28,27 @@ namespace Lombiq.Tests.UI.Services
                 // with trusted code (i.e. our own).
                 options.AddArgument("no-sandbox");
 
-                return new ChromeDriver(".", options, pageLoadTimeout).SetCommonTimeouts(pageLoadTimeout);
-            });
+                return new ChromeDriver(ChromeDriverService.CreateDefaultService(), options, pageLoadTimeout).SetCommonTimeouts(pageLoadTimeout);
+            }, new ChromeConfig());
 
         public static EdgeDriver CreateEdgeDriver(TimeSpan pageLoadTimeout) =>
-            WrapIntoException(() =>
-                new EdgeDriver(
-                    EdgeDriverService.CreateDefaultService(".", "msedgedriver.exe"),
-                    new EdgeOptions().SetCommonOptions())
-                .SetCommonTimeouts(pageLoadTimeout));
+            CreateDriver(
+                () => new EdgeDriver(new EdgeOptions().SetCommonOptions()).SetCommonTimeouts(pageLoadTimeout),
+                new EdgeConfig());
 
         public static FirefoxDriver CreateFirefoxDriver(TimeSpan pageLoadTimeout) =>
-            WrapIntoException(() => new FirefoxDriver(".", new FirefoxOptions().SetCommonOptions()).SetCommonTimeouts(pageLoadTimeout));
+            CreateDriver(
+                () => new FirefoxDriver(new FirefoxOptions().SetCommonOptions()).SetCommonTimeouts(pageLoadTimeout),
+                new FirefoxConfig());
 
         public static InternetExplorerDriver CreateInternetExplorerDriver(TimeSpan pageLoadTimeout) =>
-            WrapIntoException(() =>
+            CreateDriver(() =>
             {
                 var options = new InternetExplorerOptions().SetCommonOptions();
                 // IE doesn't support this.
                 options.AcceptInsecureCertificates = false;
-                return new InternetExplorerDriver(".", options).SetCommonTimeouts(pageLoadTimeout);
-            });
+                return new InternetExplorerDriver(options).SetCommonTimeouts(pageLoadTimeout);
+            }, new InternetExplorerConfig());
 
 
         private static TDriverOptions SetCommonOptions<TDriverOptions>(this TDriverOptions driverOptions) where TDriverOptions : DriverOptions
@@ -63,10 +70,19 @@ namespace Lombiq.Tests.UI.Services
             return driver;
         }
 
-        private static TDriver WrapIntoException<TDriver>(Func<TDriver> driverFactory) where TDriver : RemoteWebDriver
+        private static TDriver CreateDriver<TDriver>(Func<TDriver> driverFactory, IDriverConfig driverConfig) where TDriver : RemoteWebDriver
         {
             try
             {
+                // While SetUpDriver() does locking and caches the driver it's faster not to do any of that if the setup
+                // was already done. For 100 such calls it's around 16 s vs <100 ms.
+                // The Lazy<T> trick taken from: https://stackoverflow.com/a/31637510/220230
+                _ = _driverSetups.GetOrAdd(driverConfig.GetName(), _ => new Lazy<bool>(() =>
+                {
+                    new DriverManager().SetUpDriver(driverConfig);
+                    return true;
+                })).Value;
+
                 return driverFactory();
             }
             catch (Exception ex)
