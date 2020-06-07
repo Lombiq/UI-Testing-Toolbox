22a937500fa54c4ffd9b6bf91d4839a1fc4cdb2e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-06-07 18:01:17 +0200

    Adding ClickAndAssertNoPageChanges() extension for Atata Controls

---
+++ Extensions/ControlExtensions.cs
@@ -0,0 +1,23 @@
+using Atata;
+
+namespace Lombiq.Tests.UI.Extensions
+{
+    public static class ControlExtensions
+    {
+        public static TOwner ClickAndAssertNoPageChanges<TOwner>(this Control<TOwner> control)
+            where TOwner : PageObject<TOwner>
+        {
+            var pageObject = control.Owner;
+
+            string savedUrl = pageObject.PageUrl;
+            string savedHtml = pageObject.PageSource;
+
+            control.Click();
+
+            pageObject.PageUrl.Should.AtOnce.Equal(savedUrl);
+            pageObject.PageSource.Should.AtOnce.Satisfy(value => value == savedHtml, "equal previous HTML");
+
+            return pageObject;
+        }
+    }
+}
