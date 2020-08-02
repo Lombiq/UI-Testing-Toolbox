fbc8326298edd873091fe4516d1969b63ede9386
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-08-02 17:58:59 +0200

    Suppressing incorrect attribute naming warning

--- Lombiq.Tests.UI/Attributes/BrowserAttributeBase.cs
+++ Lombiq.Tests.UI/Attributes/BrowserAttributeBase.cs
@@ -1,12 +1,17 @@
 using Lombiq.Tests.UI.Services;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Xunit.Sdk;
 
 namespace Lombiq.Tests.UI.Attributes
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    [SuppressMessage(
+        "Minor Code Smell",
+        "S3376:Attribute, EventArgs, and Exception type names should end with the type being extended",
+        Justification = "It's a base class so should be suffixed with \"Base\".")]
     public abstract class BrowserAttributeBase : DataAttribute
     {
         protected abstract Browser Browser { get; }
