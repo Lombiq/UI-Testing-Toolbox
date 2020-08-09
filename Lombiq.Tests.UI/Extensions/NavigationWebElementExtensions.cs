d837333291e02c9618b0a4035fee6701e431a730
Dávid El-Saig <david.el-saig@lombiq.com>
2020-08-09 17:15:40 +0200

    Altered ClickNewContentItem to support non-dropdown buttons.

--- Lombiq.Tests.UI/Extensions/NavigationWebElementExtensions.cs
+++ Lombiq.Tests.UI/Extensions/NavigationWebElementExtensions.cs
@@ -62,10 +62,17 @@ namespace Lombiq.Tests.UI.Extensions
                 }
             }, timeout, interval);
 
-        public static void ClickNewContentItem(this UITestContext context, string contentItemName)
+        public static void ClickNewContentItem(this UITestContext context, string contentItemName, bool dropdown = true)
         {
-            context.Get(By.Id("new-dropdown")).ClickReliably(context);
-            context.Get(By.LinkText(contentItemName)).ClickReliably(context);
+            if (dropdown)
+            {
+                context.Get(By.Id("new-dropdown")).ClickReliably(context);
+                context.Get(By.LinkText(contentItemName)).ClickReliably(context);
+            }
+            else
+            {
+                context.Get(By.LinkText($"New {contentItemName}")).ClickReliably(context);
+            }
         }
 
         public static void GoToUsers(this UITestContext context)
