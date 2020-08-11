e152a5ad2f8ca522649e9a211b25e2e33b3f063f
Gábor Pór <gabor.por@lombiq.com>
2020-08-11 22:59:26 +0200

    Adding more test files

--- Lombiq.Tests.UI/Helpers/FileUploadHelper.cs
+++ Lombiq.Tests.UI/Helpers/FileUploadHelper.cs
@@ -9,5 +9,7 @@ namespace FLombiq.Tests.UI.Helpers
 
         public static readonly string SamplePdfPath = Path.Combine(BasePath, "Document.pdf");
         public static readonly string SamplePngPath = Path.Combine(BasePath, "Image.png");
+        public static readonly string SampleDocxPath = Path.Combine(BasePath, "UploadingTestFileDOCX.docx");
+        public static readonly string SampleXlsxPath = Path.Combine(BasePath, "UploadingTestFileXLSX.xlsx");
     }
 }
