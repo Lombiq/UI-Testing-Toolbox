bc2577432159627253fa0657f8f9e389aee69380
Hajdu Renáta <renata.hajdu@lombiq.com>
2020-09-08 12:29:55 +0200

    Dashboard page test fixed.

--- Lombiq.Tests.UI/Pages/OrchardCoreDashboardPage.cs
+++ Lombiq.Tests.UI/Pages/OrchardCoreDashboardPage.cs
@@ -4,9 +4,16 @@ namespace Lombiq.Tests.UI.Pages
 {
     using _ = OrchardCoreDashboardPage;
 
-    [ControlDefinition("div[contains(@class, 'menu-admin')]")]
     [Url("Admin")]
     public class OrchardCoreDashboardPage : Page<_>
     {
+        [FindByClass("menu-admin")]
+        public Content<_, _> SomeAdminContent { get; private set; }
+
+        protected override void OnVerify()
+        {
+            base.OnVerify();
+            SomeAdminContent.Should.Exist();
+        }
     }
 }
