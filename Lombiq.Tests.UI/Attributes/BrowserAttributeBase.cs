0a16e3099dce72171cf25e50ee3f9e018f45c778
Dávid El-Saig <david.el-saig@lombiq.com>
2020-10-13 21:44:24 +0200

    Clean up unneeded SuppressMessage attributes.

--- Lombiq.Tests.UI/Attributes/BrowserAttributeBase.cs
+++ Lombiq.Tests.UI/Attributes/BrowserAttributeBase.cs
@@ -8,10 +8,6 @@ using Xunit.Sdk;
 namespace Lombiq.Tests.UI.Attributes
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-    [SuppressMessage(
-        "Minor Code Smell",
-        "S3376:Attribute, EventArgs, and Exception type names should end with the type being extended",
-        Justification = "It's a base class so should be suffixed with \"Base\".")]
     public abstract class BrowserAttributeBase : DataAttribute
     {
         protected abstract Browser Browser { get; }
