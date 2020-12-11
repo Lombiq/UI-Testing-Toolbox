8f9c917c41544623b3ad7babad927443e3489d3a
Dávid El-Saig <david.el-saig@lombiq.com>
2020-12-11 23:38:19 +0100

    Replace with NonSecurityRandomizer.

--- Lombiq.Tests.UI/Services/PortLeaseManager.cs
+++ Lombiq.Tests.UI/Services/PortLeaseManager.cs
@@ -1,3 +1,4 @@
+using Lombiq.HelpfulLibraries.Libraries.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,10 +25,7 @@ namespace Lombiq.Tests.UI.Services
             lock (_portAcquisitionLock)
             {
                 var availablePorts = _availablePortsRange.Except(_usedPorts).ToList();
-                // This is not a security-critical random number.
-#pragma warning disable CA5394 // Do not use insecure randomness
-                var port = availablePorts[new Random().Next(availablePorts.Count)];
-#pragma warning restore CA5394 // Do not use insecure randomness
+                var port = availablePorts[new NonSecurityRandomizer().GetFromRange(availablePorts.Count)];
                 _usedPorts.Add(port);
                 return port;
             }
