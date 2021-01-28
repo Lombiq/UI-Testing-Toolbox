8f35dd496121ad2545cd9203825227c28cf6994d
Márk Bartha <mark.bartha@lombiq.com>
2021-01-28 22:54:08 +0100

    Adding DoWithRetriesUntilMissing

--- Lombiq.Tests.UI/Extensions/ReliabilityUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/ReliabilityUITestContextExtensions.cs
@@ -8,6 +8,14 @@ namespace Lombiq.Tests.UI.Extensions
 {
     public static class ReliabilityUITestContextExtensions
     {
+        /// <summary>
+        /// Trying to execute an operation until the given element exists.
+        /// </summary>
+        /// <param name="process">Operation to execute.</param>
+        /// <param name="elementToWaitFor">Selector of the element that's required to exist.</param>
+        /// <param name="timeout">Timeout of the operation.</param>
+        /// <param name="interval">Time between retries.</param>
+        /// <param name="existsTimeout">Timeout of checking the existence of the given element.</param>
         public static void DoWithRetriesUntilExists(
             this UITestContext context,
             Action process,
@@ -25,6 +33,37 @@ namespace Lombiq.Tests.UI.Extensions
                     existsTimeout ??= (timeout ?? TimeoutConfiguration.Default.RetryTimeout) / 5;
 
                     return context.Exists(elementToWaitFor.Safely().Within(
+                        existsTimeout.Value,
+                        interval ?? TimeoutConfiguration.Default.RetryInterval));
+                },
+                timeout,
+                interval);
+
+        /// <summary>
+        /// Trying to execute an operation until the given element goes missing.
+        /// </summary>
+        /// <param name="process">Operation to execute.</param>
+        /// <param name="elementToWaitForGoMissing">Selector of the element that's required to go missing.</param>
+        /// <param name="timeout">Timeout of the operation.</param>
+        /// <param name="interval">Time between retries.</param>
+        /// <param name="existsTimeout">Timeout of checking the existence of the given element.</param>
+        public static void DoWithRetriesUntilMissing(
+            this UITestContext context,
+            Action process,
+            By elementToWaitForGoMissing,
+            TimeSpan? timeout = null,
+            TimeSpan? interval = null,
+            TimeSpan? existsTimeout = null) =>
+            ReliabilityHelper.DoWithRetries(
+                () =>
+                {
+                    process();
+
+                    // The timeout for this existence check needs to be significantly smaller than the timeout of the
+                    // whole retry logic so actually multiple tries can happen.
+                    existsTimeout ??= (timeout ?? TimeoutConfiguration.Default.RetryTimeout) / 5;
+
+                    return context.Missing(elementToWaitForGoMissing.Safely().Within(
                         existsTimeout.Value,
                         interval ?? TimeoutConfiguration.Default.RetryInterval));
                 },
