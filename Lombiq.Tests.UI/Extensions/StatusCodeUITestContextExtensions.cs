6ad2d05e5d032394e7576e87ef11a9c09b857001
Márk Bartha <mark.bartha@lombiq.com>
2021-01-28 01:58:48 +0100

    Adding StatusCodeUITestContextExtensions

---
+++ Lombiq.Tests.UI/Extensions/StatusCodeUITestContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Lombiq.Tests.UI.Services;
+using Shouldly;
+
+namespace Lombiq.Tests.UI.Extensions
+{
+    public static class StatusCodeUITestContextExtensions
+    {
+        public static void AssertStatusCodeOnUrl(this UITestContext context, string url, int statusCode) =>
+            context.ExecuteAsyncScript(
+                @"var url = arguments[0];
+                var callback = arguments[arguments.length - 1];
+                fetch(url).then(function(response) {
+                    callback(response.status);
+                });",
+                url).ShouldBe(statusCode);
+
+        public static void AssertNotFoundResultOnUrl(this UITestContext context, string url) =>
+            context.AssertStatusCodeOnUrl(url, 404);
+
+        public static void AssertSuccessResultOnUrl(this UITestContext context, string url) =>
+            context.AssertStatusCodeOnUrl(url, 200);
+    }
+}
