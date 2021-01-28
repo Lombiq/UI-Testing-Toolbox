f2970654b00f4ab0cb437e8ca1cae28e2bdb9bcb
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-01-28 20:59:48 +0100

    Elaborating comments

--- Lombiq.Tests.UI/Extensions/StatusCodeUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/StatusCodeUITestContextExtensions.cs
@@ -6,7 +6,7 @@ namespace Lombiq.Tests.UI.Extensions
     public static class StatusCodeUITestContextExtensions
     {
         /// <summary>
-        /// Opens the given URL asynchronously and checks the response code.
+        /// Opens the given URL asynchronously and checks the HTTP response code.
         /// </summary>
         /// <param name="url">Relative URL to open.</param>
         /// <param name="statusCode">Status code to assert.</param>
@@ -21,14 +21,14 @@ namespace Lombiq.Tests.UI.Extensions
             .ShouldBe(statusCode);
 
         /// <summary>
-        /// Opens the given URL asynchronously and checks if the response code is 404.
+        /// Opens the given URL asynchronously and checks if the HTTP response code is 404.
         /// </summary>
         /// <param name="url">Relative URL to open.</param>
         public static void AssertNotFoundResultOnUrl(this UITestContext context, string url) =>
             context.AssertStatusCodeOnUrl(url, 404);
 
         /// <summary>
-        /// Opens the given URL asynchronously and checks if the response code is 200.
+        /// Opens the given URL asynchronously and checks if the HTTP response code is 200.
         /// </summary>
         /// <param name="url">Relative URL to open.</param>
         public static void AssertSuccessResultOnUrl(this UITestContext context, string url) =>
