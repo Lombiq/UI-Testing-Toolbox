04ae0cc1a00579bcd98bcac98d4e91777f254324
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-03 23:36:33 +0100

    Added GetTextTrimmed method.

---
+++ Lombiq.Tests.UI/Extensions/BasicWebElementExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenQA.Selenium
+{
+    public static class BasicWebElementExtensions
+    {
+        /// <summary>
+        /// Returns the text content of the <paramref name="element"/> without surrounding whitespace.
+        /// </summary>
+        public static string GetTextTrimmed(this IWebElement element) => element.Text.Trim();
+    }
+}
