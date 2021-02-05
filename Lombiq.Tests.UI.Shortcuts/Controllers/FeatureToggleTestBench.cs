5bdd3444f345fcccd063e7f00997ddd4e912f71a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-02-05 18:41:07 +0100

    Suppressing incorrect analyzer violation

--- Lombiq.Tests.UI.Shortcuts/Controllers/FeatureToggleTestBench.cs
+++ Lombiq.Tests.UI.Shortcuts/Controllers/FeatureToggleTestBench.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Modules;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lombiq.Tests.UI.Shortcuts.Controllers
 {
     [Feature(ShortcutsFeatureIds.FeatureToggleTestBench)]
     public class FeatureToggleTestBench : Controller
     {
+        // While the warning doesn't show up in VS it does with dotnet build.
+#pragma warning disable IDE0079 // Remove unnecessary suppression
+        [SuppressMessage(
+            "Performance",
+            "CA1822:Mark members as static",
+            Justification = "It's a controller action that needs to be instance-level.")]
+#pragma warning restore IDE0079 // Remove unnecessary suppression
         public string Index() => "The Feature Toggle Test Bench worked.";
     }
 }
