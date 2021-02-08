6ecd0a9bbbfedde6f0bdb608ad1ae1d4a1d0d9bf
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-02-08 23:07:16 +0100

    Fixing "FormatException : Input string was not in a correct format." exceptions when exceptions were logged

--- Lombiq.Tests.UI/Extensions/TestOutputHelperExtensions.cs
+++ Lombiq.Tests.UI/Extensions/TestOutputHelperExtensions.cs
@@ -10,7 +10,12 @@ namespace Xunit.Abstractions
             DebugHelper.WriteLineTimestamped(format, args);
         }
 
-        public static void WriteLineTimestamped(this ITestOutputHelper testOutputHelper, string format, params object[] args) =>
-            testOutputHelper.WriteLine(DebugHelper.PrefixWithTimestamp(format), args);
+        public static void WriteLineTimestamped(this ITestOutputHelper testOutputHelper, string format, params object[] args)
+        {
+            // Preventing "FormatException : Input string was not in a correct format." exceptions if the message
+            // contains characters used in string formatting but it shouldn't actually be formatted.
+            if (args == null || args.Length == 0) testOutputHelper.WriteLine(DebugHelper.PrefixWithTimestamp(format));
+            else testOutputHelper.WriteLine(DebugHelper.PrefixWithTimestamp(format), args);
+        }
     }
 }
--- Lombiq.Tests.UI/Helpers/DebugHelper.cs
+++ Lombiq.Tests.UI/Helpers/DebugHelper.cs
@@ -6,8 +6,13 @@ namespace Lombiq.Tests.UI.Helpers
 {
     public static class DebugHelper
     {
-        public static void WriteLineTimestamped(string format, params object[] args) =>
-            Debug.WriteLine(PrefixWithTimestamp(format), args);
+        public static void WriteLineTimestamped(string format, params object[] args)
+        {
+            // Preventing "FormatException : Input string was not in a correct format." exceptions if the message
+            // contains characters used in string formatting but it shouldn't actually be formatted.
+            if (args == null || args.Length == 0) Debug.WriteLine(PrefixWithTimestamp(format));
+            else Debug.WriteLine(PrefixWithTimestamp(format), args);
+        }
 
         public static string PrefixWithTimestamp(string message) =>
             $"{DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)} - {message}";
