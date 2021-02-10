a143a27a59cf38d744bb38cfd58d52082506b9c6
Dávid El-Saig <david.el-saig@lombiq.com>
2021-02-10 13:03:06 +0100

    Added SmtpInboxRow.

---
+++ Lombiq.Tests.UI/Helpers/ByHelper.cs
@@ -0,0 +1,21 @@
+﻿using Atata;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using System;
+
+namespace Lombiq.Tests.UI.Helpers
+{
+    /// <summary>
+    /// Provides helper functions for generating <see cref="By"/> selectors.
+    /// </summary>
+    public static class ByHelper
+    {
+        /// <summary>
+        /// Returns an XPath selector for an email in the list with the given subject.
+        /// </summary>
+        public static By SmtpInboxRow(string subject) =>
+            By
+                .XPath($"//tr[contains(@class,'el-table__row')]//div[contains(@class,'cell')][contains(text(), {JsonConvert.SerializeObject(subject)})]")
+                .Within(TimeSpan.FromMinutes(2));
+    }
+}
