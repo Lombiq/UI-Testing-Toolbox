d5cffe5e3cc023d6d061cddeec29611790a9f5e1
Krisztián Németh <krisztian.nemeth@lombiq.com>
2021-02-11 15:45:09 +0100

    Testing new azure blob snapshot creation method.

--- Lombiq.Tests.UI/Services/AzureBlobStorageManager.cs
+++ Lombiq.Tests.UI/Services/AzureBlobStorageManager.cs
@@ -75,16 +75,7 @@ namespace Lombiq.Tests.UI.Services
             var mediaFolderPath = GetMediaFolderPath(snapshotDirectoryPath);
 
             DirectoryHelper.CreateDirectoryIfNotExists(mediaFolderPath);
-
-            return IterateThroughBlobsAsync(
-                blobClient =>
-                {
-                    var blobUrl = blobClient.Name[(blobClient.Name.IndexOf('/', StringComparison.OrdinalIgnoreCase) + 1)..];
-                    var blobPath = blobUrl.Replace("/", Path.DirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase);
-                    var blobFullPath = Path.Combine(mediaFolderPath, blobPath);
-                    DirectoryHelper.CreateDirectoryIfNotExists(Path.GetDirectoryName(blobFullPath));
-                    return blobClient.DownloadToAsync(blobFullPath);
-                });
+            return CreateSnapShot(mediaFolderPath);
         }
 
         public async Task RestoreSnapshotAsync(string snapshotDirectoryPath)
@@ -121,6 +112,22 @@ namespace Lombiq.Tests.UI.Services
             await foreach (var blob in page)
             {
                 await blobProcessor(_blobContainer.GetBlobClient(blob.Name));
+            }
+        }
+        private async Task CreateSnapShot(string mediaFolderPath)
+        {
+            var page = _blobContainer.GetBlobsAsync(BlobTraits.Metadata, BlobStates.None, _basePath).AsPages();
+            await foreach (var blob in page)
+            {
+                foreach (var blobItem in blob.Values)
+                {
+                    var blobClient = _blobContainer.GetBlobClient(blobItem.Name);
+                    var blobUrl = blobClient.Name[(blobClient.Name.IndexOf('/', StringComparison.OrdinalIgnoreCase) + 1)..];
+                    var blobPath = blobUrl.Replace("/", Path.DirectorySeparatorChar.ToString(), StringComparison.OrdinalIgnoreCase);
+                    var blobFullPath = Path.Combine(mediaFolderPath, blobPath);
+                    DirectoryHelper.CreateDirectoryIfNotExists(Path.GetDirectoryName(blobFullPath));
+                    await blobClient.DownloadToAsync(blobFullPath);
+                }
             }
         }
 
--- Lombiq.Tests.UI/Services/OrchardCoreInstance.cs
+++ Lombiq.Tests.UI/Services/OrchardCoreInstance.cs
@@ -113,7 +113,7 @@ namespace Lombiq.Tests.UI.Services
 
             Directory.CreateDirectory(snapshotDirectoryPath);
 
-            _configuration.BeforeTakeSnapshot?.Invoke(_contentRootPath, snapshotDirectoryPath);
+            await _configuration.BeforeTakeSnapshot?.Invoke(_contentRootPath, snapshotDirectoryPath);
 
             FileSystem.CopyDirectory(_contentRootPath, snapshotDirectoryPath, true);
         }
