214666935a57970ff44d6dc8806fa3bf2c20d4fc
Márk Bartha <mark.bartha@lombiq.com>
2021-02-25 17:11:33 +0100

    Adding WaitUntilExists and WaitUntilMissing helpers

--- Lombiq.Tests.UI/Extensions/ReliabilityUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/ReliabilityUITestContextExtensions.cs
@@ -28,13 +28,9 @@ namespace Lombiq.Tests.UI.Extensions
                 {
                     process();
 
-                    // The timeout for this existence check needs to be significantly smaller than the timeout of the
-                    // whole retry logic so actually multiple tries can happen.
-                    existsTimeout ??= (timeout ?? TimeoutConfiguration.Default.RetryTimeout) / 5;
+                    existsTimeout ??= GetExistsTimeout(timeout);
 
-                    return context.Exists(elementToWaitFor.Safely().Within(
-                        existsTimeout.Value,
-                        interval ?? TimeoutConfiguration.Default.RetryInterval));
+                    return ExistsWithin(context, elementToWaitFor, existsTimeout.Value, interval);
                 },
                 timeout,
                 interval);
@@ -59,15 +55,80 @@ namespace Lombiq.Tests.UI.Extensions
                 {
                     process();
 
-                    // The timeout for this existence check needs to be significantly smaller than the timeout of the
-                    // whole retry logic so actually multiple tries can happen.
-                    existsTimeout ??= (timeout ?? TimeoutConfiguration.Default.RetryTimeout) / 5;
+                    existsTimeout ??= GetExistsTimeout(timeout);
 
-                    return context.Missing(elementToWaitForGoMissing.Safely().Within(
-                        existsTimeout.Value,
-                        interval ?? TimeoutConfiguration.Default.RetryInterval));
+                    return MissingWithin(context, elementToWaitForGoMissing, existsTimeout.Value, interval);
                 },
                 timeout,
                 interval);
+
+        /// <summary>
+        /// Waits until the given element exists.
+        /// </summary>
+        /// <param name="elementToWaitFor">Selector of the element that's required to exist.</param>
+        /// <param name="timeout">Timeout of the operation.</param>
+        /// <param name="interval">Time between retries.</param>
+        /// <param name="existsTimeout">Timeout of checking the existence of the given element.</param>
+        public static void WaitUntilExists(
+            this UITestContext context,
+            By elementToWaitFor,
+            TimeSpan? timeout = null,
+            TimeSpan? interval = null,
+            TimeSpan? existsTimeout = null) =>
+            ReliabilityHelper.DoWithRetries(
+                () =>
+                {
+                    existsTimeout ??= GetExistsTimeout(timeout);
+
+                    return ExistsWithin(context, elementToWaitFor, existsTimeout.Value, interval);
+                },
+                timeout,
+                interval);
+
+        /// <summary>
+        /// Waits until the given element goes missing.
+        /// </summary>
+        /// <param name="elementToWaitForGoMissing">Selector of the element that's required to go missing.</param>
+        /// <param name="timeout">Timeout of the operation.</param>
+        /// <param name="interval">Time between retries.</param>
+        /// <param name="existsTimeout">Timeout of checking the existence of the given element.</param>
+        public static void WaitUntilMissing(
+            this UITestContext context,
+            By elementToWaitForGoMissing,
+            TimeSpan? timeout = null,
+            TimeSpan? interval = null,
+            TimeSpan? existsTimeout = null) =>
+            ReliabilityHelper.DoWithRetries(
+                () =>
+                {
+                    existsTimeout ??= GetExistsTimeout(timeout);
+
+                    return MissingWithin(context, elementToWaitForGoMissing, existsTimeout.Value, interval);
+                },
+                timeout,
+                interval);
+
+        private static TimeSpan GetExistsTimeout(TimeSpan? timeout) =>
+            // The timeout for this existence check needs to be significantly smaller than the timeout of the
+            // whole retry logic so actually multiple tries can happen.
+            (timeout ?? TimeoutConfiguration.Default.RetryTimeout) / 5;
+
+        private static bool ExistsWithin(
+            UITestContext context,
+            By elementToWaitFor,
+            TimeSpan existsTimeout,
+            TimeSpan? interval = null) =>
+            context.Exists(elementToWaitFor.Safely().Within(
+                existsTimeout,
+                interval ?? TimeoutConfiguration.Default.RetryInterval));
+
+        private static bool MissingWithin(
+            UITestContext context,
+            By elementToWaitForGoMissing,
+            TimeSpan existsTimeout,
+            TimeSpan? interval = null) =>
+            context.Missing(elementToWaitForGoMissing.Safely().Within(
+                existsTimeout,
+                interval ?? TimeoutConfiguration.Default.RetryInterval));
     }
 }
