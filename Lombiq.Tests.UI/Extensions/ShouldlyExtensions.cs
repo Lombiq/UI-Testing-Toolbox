0f29f362ba373ebd4f8311056c1c818d3df66128
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-12 14:53:25 +0100

    Added warning name to SuppressMessage.

--- Lombiq.Tests.UI/Extensions/ShouldlyExtensions.cs
+++ Lombiq.Tests.UI/Extensions/ShouldlyExtensions.cs
@@ -8,7 +8,7 @@ namespace Shouldly
         /// Calls <see cref="object.ToString()"/> on both <paramref name="actual"/> and <paramref name="expected"/>
         /// (unless they are <see langword="null"/>) and checks if they are the same.
         /// </summary>
-        [SuppressMessage("Code Smell", "S4225", Justification = "This is what Shouldly does.")]
+        [SuppressMessage("Code Smell", "S4225:Extension methods should not extend \"object\"", Justification = "This is what Shouldly does.")]
         public static void ShouldBeAsString(this object actual, object expected, string customMessage = null)
         {
             // We need this variable because the null-forgiving operator is shortcutting. This way the ShouldBe is
