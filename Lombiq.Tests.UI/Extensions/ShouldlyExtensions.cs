1bc262769c055ba94d7ae96e0e36b645ff91dc20
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-12 14:20:17 +0100

    Suppress senseless complaint.

--- Lombiq.Tests.UI/Extensions/ShouldlyExtensions.cs
+++ Lombiq.Tests.UI/Extensions/ShouldlyExtensions.cs
@@ -1,11 +1,14 @@
-﻿namespace Shouldly
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Shouldly
 {
     public static class ShouldlyExtensions
     {
         /// <summary>
         /// Calls <see cref="object.ToString()"/> on both <paramref name="actual"/> and <paramref name="expected"/>
-        /// (unless they are <see langword="null"/>) and
+        /// (unless they are <see langword="null"/>) and checks if they are the same.
         /// </summary>
+        [SuppressMessage("Code Smell", "S4225", Justification = "This is what Shouldly does.")]
         public static void ShouldBeAsString(this object actual, object expected, string customMessage = null)
         {
             // We need this variable because the null-forgiving operator is shortcutting. This way the ShouldBe is
