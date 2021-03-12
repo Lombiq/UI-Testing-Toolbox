d5ad8351024f76d13a1510f02cc744a62cb96234
Dávid El-Saig <david.el-saig@lombiq.com>
2021-03-12 14:15:15 +0100

    Revert "Revert "Added ShouldBeAsString.""
    This reverts commit cee697ae39cccb31573375665117fcca2535901b.

---
+++ Lombiq.Tests.UI/Extensions/ShouldlyExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Shouldly
+{
+    public static class ShouldlyExtensions
+    {
+        /// <summary>
+        /// Calls <see cref="object.ToString()"/> on both <paramref name="actual"/> and <paramref name="expected"/>
+        /// (unless they are <see langword="null"/>) and
+        /// </summary>
+        public static void ShouldBeAsString(this object actual, object expected, string customMessage = null)
+        {
+            // We need this variable because the null-forgiving operator is shortcutting. This way the ShouldBe is
+            // called even if actual is null.
+            var actualText = actual?.ToString();
+
+            actualText.ShouldBe(expected?.ToString(), customMessage);
+        }
+    }
+}
