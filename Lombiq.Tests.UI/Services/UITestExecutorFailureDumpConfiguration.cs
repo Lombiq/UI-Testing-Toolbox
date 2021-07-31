4154df536c823cdc4fb1477feb22f75604e97362
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-07-31 22:32:49 +0200

    Only defaulting to using short failure dump folder names on Windows

--- Lombiq.Tests.UI/Services/UITestExecutorFailureDumpConfiguration.cs
+++ Lombiq.Tests.UI/Services/UITestExecutorFailureDumpConfiguration.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Lombiq.Tests.UI.Services
 {
     public class UITestExecutorFailureDumpConfiguration
@@ -5,9 +7,10 @@ namespace Lombiq.Tests.UI.Services
         /// <summary>
         /// Gets or sets a value indicating whether the subfolder of each test's dumps will use a shortened name, only
         /// containing the name of the test method, without the name of the test class and its namespace. This is to
-        /// overcome the 260 character path length limitations on Windows. Defaults to <see langword="true"/>.
+        /// overcome the 260 character path length limitations on Windows. Defaults to <see langword="true"/> on
+        /// Windows.
         /// </summary>
-        public bool UseShortNames { get; set; } = true;
+        public bool UseShortNames { get; set; } = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
         public string DumpsDirectoryPath { get; set; } = "FailureDumps";
         public bool CaptureAppSnapshot { get; set; } = true;
