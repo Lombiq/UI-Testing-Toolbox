a59338af9618c131618d45a9f7f13a8be3fff2a4
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-02 19:16:05 +0100

    Add error controller and refactor ShortcutsUITestContextExtensions.

---
+++ Lombiq.Tests.UI.Shortcuts/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Lombiq.HelpfulLibraries.Libraries.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Lombiq.Tests.UI.Shortcuts.Controllers
+{
+    [DevelopmentAndLocalhostOnly]
+    public class ErrorController : Controller
+    {
+
+        [AllowAnonymous]
+        public IActionResult Index() =>
+            throw new InvalidOperationException("This action causes an exception!");
+    }
+}
