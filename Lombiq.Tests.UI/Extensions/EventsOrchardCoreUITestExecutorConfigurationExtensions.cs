5b3d94538ab2c32704c0766c1496bc4ac10b40bb
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-03 19:59:49 +0100

    DRY SetUpEvents.

--- Lombiq.Tests.UI/Extensions/EventsOrchardCoreUITestExecutorConfigurationExtensions.cs
+++ Lombiq.Tests.UI/Extensions/EventsOrchardCoreUITestExecutorConfigurationExtensions.cs
@@ -11,31 +11,9 @@ namespace Lombiq.Tests.UI.Extensions
         {
             if (!configuration.CustomConfiguration.TryAdd("EventsWereSetUp", true)) return;
 
-            static bool IsNoAlert(UITestContext context)
-            {
-                // If there's an alert (which can happen mostly after a click but also after navigating) then all other
-                // driver operations, even retrieving the current URL, will throw an UnhandledAlertException. Thus we
-                // need to check if an alert is present and that's only possible by catching exceptions.
-                try
-                {
-                    context.Driver.SwitchTo().Alert();
-                    return false;
-                }
-                catch (NoAlertPresentException)
-                {
-                    return true;
-                }
-            }
-
             PageNavigationState navigationState = null;
 
-            configuration.Events.AfterNavigation += async (context, _) =>
-            {
-                if (IsNoAlert(context) && configuration.Events.AfterPageChange != null)
-                {
-                    await configuration.Events.AfterPageChange.Invoke(context);
-                }
-            };
+            configuration.Events.AfterNavigation += (context, _) => OnEventsAfterNavigationAsync(context);
 
             configuration.Events.BeforeClick += (context, _) =>
             {
@@ -43,15 +21,31 @@ namespace Lombiq.Tests.UI.Extensions
                 return Task.CompletedTask;
             };
 
-            configuration.Events.AfterClick += async (context, _) =>
-            {
-                if (IsNoAlert(context) &&
-                    navigationState.CheckIfNavigationHasOccurred() &&
-                    configuration.Events.AfterPageChange != null)
-                {
-                    await configuration.Events.AfterPageChange.Invoke(context);
-                }
-            };
+            configuration.Events.AfterClick += (context, _) =>
+                navigationState.CheckIfNavigationHasOccurred()
+                    ? OnEventsAfterNavigationAsync(context)
+                    : Task.CompletedTask;
         }
+
+        private static bool IsNoAlert(UITestContext context)
+        {
+            // If there's an alert (which can happen mostly after a click but also after navigating) then all other
+            // driver operations, even retrieving the current URL, will throw an UnhandledAlertException. Thus we
+            // need to check if an alert is present and that's only possible by catching exceptions.
+            try
+            {
+                context.Driver.SwitchTo().Alert();
+                return false;
+            }
+            catch (NoAlertPresentException)
+            {
+                return true;
+            }
+        }
+
+        private static Task OnEventsAfterNavigationAsync(UITestContext context) =>
+            IsNoAlert(context) && context.Configuration.Events.AfterPageChange is { } afterPageChange
+                ? afterPageChange.Invoke(context)
+                : Task.CompletedTask;
     }
 }
