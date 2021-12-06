3d9412b3dacb7f10208e8efb87da7438680b2d6f
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-12-06 01:42:00 +0100

    Fixing nonsensical analyzer violation

--- Lombiq.Tests.UI.Shortcuts/Controllers/ErrorController.cs
+++ Lombiq.Tests.UI.Shortcuts/Controllers/ErrorController.cs
@@ -1,7 +1,8 @@
-﻿using Lombiq.HelpfulLibraries.Libraries.Mvc;
+using Lombiq.HelpfulLibraries.Libraries.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lombiq.Tests.UI.Shortcuts.Controllers
 {
@@ -10,6 +11,8 @@ namespace Lombiq.Tests.UI.Shortcuts.Controllers
     {
         public const string ExceptionMessage = "This action causes an exception!";
 
+        // This only happens in the CI for some reason.
+        [SuppressMessage("Usage", "CA1822:Mark members as static", Justification = "It's a controller action.")]
         [AllowAnonymous]
         public IActionResult Index() =>
             throw new InvalidOperationException(ExceptionMessage);
