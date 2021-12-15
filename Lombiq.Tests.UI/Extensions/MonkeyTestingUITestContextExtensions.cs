a05d1c7e4262382055c912a1467224f7758a5ecd
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2021-12-15 15:32:42 +0200

    Refactor `MonkeyTestingUITestContextExtensions` and update XML comments

--- Lombiq.Tests.UI/Extensions/MonkeyTestingUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/MonkeyTestingUITestContextExtensions.cs
@@ -9,13 +9,15 @@ namespace Lombiq.Tests.UI.Extensions
     public static class MonkeyTestingUITestContextExtensions
     {
         /// <summary>
-        /// Tests the current page as monkey.
-        /// Test finishes by timeout or when the current page is left during testing.
+        /// Tests the current page as monkey. Test finishes by timeout or when the current page is left during testing.
         /// </summary>
         /// <param name="options">The options.</param>
         /// <param name="randomSeed">The random seed.</param>
         /// <returns>The same <see cref="UITestContext"/> instance.</returns>
-        public static UITestContext TestCurrentPageAsMonkey(this UITestContext context, MonkeyTestingOptions options = null, int? randomSeed = null)
+        public static UITestContext TestCurrentPageAsMonkey(
+            this UITestContext context,
+            MonkeyTestingOptions options = null,
+            int? randomSeed = null)
         {
             var monkeyTester = new MonkeyTester(context, options);
             monkeyTester.TestOnePage(randomSeed);
@@ -24,11 +26,15 @@ namespace Lombiq.Tests.UI.Extensions
         }
 
         /// <summary>
-        /// Tests the current page as monkey recursively.
+        /// Tests the current page as monkey recursively. When current page is left during test, continues to test the
+        /// other page and so on. Each page is tested for <see cref="MonkeyTestingOptions.PageTestTime"/> value of
+        /// <paramref name="options"/>.
         /// </summary>
         /// <param name="options">The options.</param>
         /// <returns>The same <see cref="UITestContext"/> instance.</returns>
-        public static UITestContext TestCurrentPageAsMonkeyRecursively(this UITestContext context, MonkeyTestingOptions options = null)
+        public static UITestContext TestCurrentPageAsMonkeyRecursively(
+            this UITestContext context,
+            MonkeyTestingOptions options = null)
         {
             var monkeyTester = new MonkeyTester(context, options);
             monkeyTester.TestRecursively();
