ca3ec75cd85c6e97bc69c6757775e6f57e40c247
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-02-10 12:24:25 +0100

    Adding DebuggerDisplay to monkey testing URL filters and sanitizers

--- Lombiq.Tests.UI/MonkeyTesting/UrlFilters/StartsWithMonkeyTestingUrlFilter.cs
+++ Lombiq.Tests.UI/MonkeyTesting/UrlFilters/StartsWithMonkeyTestingUrlFilter.cs
@@ -1,10 +1,13 @@
 using Lombiq.Tests.UI.Services;
 using System;
+using System.Diagnostics;
+
 namespace Lombiq.Tests.UI.MonkeyTesting.UrlFilters
 {
     /// <summary>
     /// URL filter that matches the URL to see if starts with the configured relative URL.
     /// </summary>
+    [DebuggerDisplay("Starts with \"{_relativeUrlStartsWith}\"")]
     public class StartsWithMonkeyTestingUrlFilter : IMonkeyTestingUrlFilter
     {
         private readonly string _relativeUrlStartsWith;
