411559c464c2935d079200d3b251d72f7f4eb0b0
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-03-10 22:53:33 +0100

    Removing .NET 5 reference

--- Lombiq.Tests.UI/Helpers/WebAppConfigHelper.cs
+++ Lombiq.Tests.UI/Helpers/WebAppConfigHelper.cs
@@ -10,10 +10,10 @@ namespace Lombiq.Tests.UI.Helpers
         /// </summary>
         /// <param name="webAppName">The web app's project name.</param>
         /// <param name="frameworkFolderName">
-        /// The name of the folder that corresponds to the .NET version in the build output folder (e.g. "net5.0").
+        /// The name of the folder that corresponds to the .NET version in the build output folder (e.g. "net6.0").
         /// </param>
         /// <returns>The absolute path to the assembly (DLL) of the application being tested.</returns>
-        public static string GetAbsoluteApplicationAssemblyPath(string webAppName, string frameworkFolderName = "net5.0")
+        public static string GetAbsoluteApplicationAssemblyPath(string webAppName, string frameworkFolderName = "net6.0")
         {
             string baseDirectory;
 
