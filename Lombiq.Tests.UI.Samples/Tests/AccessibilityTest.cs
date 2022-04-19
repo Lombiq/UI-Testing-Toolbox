c1731b92c488467743d77205214c9e5accb08732
Dávid El-Saig <david.el-saig@lombiq.com>
2022-04-19 03:14:48 +0200

    Adjust FrontendPagesShoudlBeAccessbile

--- Lombiq.Tests.UI.Samples/Tests/AccessibilityTest.cs
+++ Lombiq.Tests.UI.Samples/Tests/AccessibilityTest.cs
@@ -33,11 +33,11 @@ public class AccessibilityTest : UITestBase
                 configuration.AccessibilityCheckingConfiguration.RunAccessibilityCheckingAssertionOnAllPageChanges = true;
 
                 // We'll check for the WCAG 2.1 AA level. This is the middle level of the latest accessibility
-                // guidelines. The frontend set up with the Blog recipe actually has a couple of issues. For the sake of
-                // this sample we won't try to fix them but rather disable the corresponding rules.
+                // guidelines. The footer widget created by the Blog recipe actually has a couple of issues. For the
+                // sake of this sample we won't try to fix them but rather disable the corresponding rules.
                 configuration.AccessibilityCheckingConfiguration.AxeBuilderConfigurator += axeBuilder =>
                     AccessibilityCheckingConfiguration.ConfigureWcag21aa(axeBuilder)
-                        .DisableRules("color-contrast", "landmark-one-main", "link-name", "region");
+                        .DisableRules("color-contrast", "link-name");
             });
 }
 
