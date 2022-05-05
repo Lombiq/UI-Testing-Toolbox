5471b2203bc5a570414184f0be54e5abcab00460
Szabolcs Deme <szabolcs.deme@lombiq.com>
2022-05-05 17:10:11 +0200

    Code styling, removing unnecessary code

--- Lombiq.Tests.UI.Samples/Constants/RecipeIds.cs
+++ Lombiq.Tests.UI.Samples/Constants/RecipeIds.cs
@@ -1,4 +1,5 @@
 namespace Lombiq.Tests.UI.Samples.Constants;
+
 public static class RecipeIds
 {
     public const string BasicOrchardFeaturesTests = "Lombiq.OSOCE.BasicOrchardFeaturesTests";
--- Lombiq.Tests.UI.Samples/Tests/DataBaseSnapshotTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/DataBaseSnapshotTests.cs
@@ -2,39 +2,39 @@ using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Samples.Constants;
 using Lombiq.Tests.UI.Services;
-using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Lombiq.Tests.UI.Samples.Tests;
 
-// We can execute test on already existing databases. To test this, we will take a snapshot of the running application,
-// and use that.
-public class DataBaseSnapshotTests : UITestBase
+// We can execute tests on already existing databases.To demo this, we will take a snapshot of the running application,
+// and use that, because we don't have a pre-beaked database. Normally this feature is used to run tests when there is
+// an already existing database, so you don't need to take a snapshot before using it.
+public class DatabaseSnapshotTests : UITestBase
 {
-    public DataBaseSnapshotTests(ITestOutputHelper testOutputHelper)
+    public DatabaseSnapshotTests(ITestOutputHelper testOutputHelper)
         : base(testOutputHelper)
     {
     }
 
     // Here, we set up the application, then we take a snapshot of it, then we use the
-    // "ExecuteTestFromExistingDBAsync()" to run the test on that. Then we test the basic Orchard features to check that
-    // the application was set up correctly.
+    // "ExecuteTestFromExistingDBAsync()" to run the test on that. Finally, we test the basic Orchard features to check
+    // that the application was set up correctly.
     [Theory, Chrome]
-    public Task BasicOrchardFeaturesShouldWorkWithExistingDBSetup(Browser browser) =>
+    public Task BasicOrchardFeaturesShouldWorkWithExistingDatabaseSetup(Browser browser) =>
          ExecuteTestAsync(
                 async context =>
                 {
-                    const string AppForDataBaseTestFolder = "AppForDataBaseTest";
+                    const string AppForDatabaseTestFolder = "AppForDatabaseTest";
 
                     await context.GoToSetupPageAndSetupOrchardCoreAsync(RecipeIds.BasicOrchardFeaturesTests);
-                    await context.Application.TakeSnapshotAsync(AppForDataBaseTestFolder);
+                    await context.Application.TakeSnapshotAsync(AppForDatabaseTestFolder);
 
                     await ExecuteTestFromExistingDBAsync(
                          async context => await context.TestBasicOrchardFeaturesExceptSetupAsync(),
                          browser,
-                         Directory.GetCurrentDirectory() + "//" + AppForDataBaseTestFolder);
+                         AppForDatabaseTestFolder);
                 },
                 browser);
 }
