72f53ddde8c8e33f68bb77e2c748e3cbdf4783e8
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-14 15:37:21 +0200

    Comment fixes.

--- Lombiq.Tests.UI.Samples/Tests/AzureBlobStorageTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/AzureBlobStorageTests.cs
@@ -9,8 +9,8 @@ using Xunit.Abstractions;
 
 namespace Lombiq.Tests.UI.Samples.Tests;
 
-// Up until now the Orchard app always used the default local Media storage for managing Media files. However, you
-// may use Azure Blob Storage in production. You can also test your app with it!
+// Up until now the Orchard app always used the default local Media storage for managing Media files. However, you  may
+// use Azure Blob Storage in production. You can also test your app with it!
 public class AzureBlobStorageTests : UITestBase
 {
     public AzureBlobStorageTests(ITestOutputHelper testOutputHelper)
@@ -19,8 +19,8 @@ public class AzureBlobStorageTests : UITestBase
     }
 
     // Here we have basically two of the same tests as in BasicTests but now we're using Azure Blob Storage as the
-    // site's Media storage. If they still work and there are no errors in the log then the app works with Azure
-    // Blob Storage too.
+    // site's Media storage. If they still work and there are no errors in the log then the app works with Azure Blob
+    // Storage too.
     [Theory, Chrome]
     public Task AnonymousHomePageShouldExistWithAzureBlobStorage(Browser browser) =>
         ExecuteTestAfterSetupAsync(
@@ -28,8 +28,8 @@ public class AzureBlobStorageTests : UITestBase
             browser,
             // Note the configuration! We could also set this globally in UITestBase.
             // You'll need an accessible Azure Blob Storage account. For testing we recommend the Azurite emulator
-            // (https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite) that can be used from
-            // tests without any further configuration.
+            // (https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azurite) that can be used from tests
+            // without any further configuration.
             configuration => configuration.UseAzureBlobStorage = true);
 
     [Theory, Chrome]
