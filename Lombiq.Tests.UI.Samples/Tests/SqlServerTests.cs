41d11abb037faca21dd66d609a73a812f8fffd04
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-14 23:51:25 +0200

    Intentional test error in SQL test.

--- Lombiq.Tests.UI.Samples/Tests/SqlServerTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/SqlServerTests.cs
@@ -2,6 +2,7 @@ using Lombiq.Tests.UI.Attributes;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Samples.Extensions;
 using Lombiq.Tests.UI.Services;
+using Microsoft.SqlServer.Management.Dmf;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -26,7 +27,11 @@ public class SqlServerTests : UITestBase
     [Theory, Chrome]
     public Task AnonymousHomePageShouldExistWithSqlServer(Browser browser) =>
         ExecuteTestAfterSetupAsync(
-            context => context.CheckIfAnonymousHomePageExistsAsync(),
+            async context =>
+            {
+                await context.CheckIfAnonymousHomePageExistsAsync();
+                throw new InvalidOperandException("Intentional test error.");
+            },
             browser,
             // Note the configuration! We could also set this globally in UITestBase.
             configuration => configuration.UseSqlServer = true);
