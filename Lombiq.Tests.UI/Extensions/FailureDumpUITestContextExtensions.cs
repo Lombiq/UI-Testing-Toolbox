01363d5c0237cd2d2ef492f58be60dc7226f2229
József Horváth <info@ministro.hu>
2022-05-28 12:28:38 +0200

    adding comments

--- Lombiq.Tests.UI/Extensions/FailureDumpUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/FailureDumpUITestContextExtensions.cs
@@ -9,6 +9,12 @@ namespace Lombiq.Tests.UI.Extensions;
 
 public static class FailureDumpUITestContextExtensions
 {
+    /// <summary>
+    /// Appends stream as file content to be collected on failure dump.
+    /// </summary>
+    /// <param name="context"><see cref="UITestContext"/> instance.</param>
+    /// <param name="fileName">The name of the file.</param>
+    /// <param name="action">Gets called in failure dump collection.</param>
     public static void AppendFailureDump(
         this UITestContext context,
         string fileName,
@@ -17,6 +23,15 @@ public static class FailureDumpUITestContextExtensions
                 fileName,
                 () => action(context));
 
+    /// <summary>
+    /// Appends string as file content to be collected on failure dump.
+    /// </summary>
+    /// <param name="context"><see cref="UITestContext"/> instance.</param>
+    /// <param name="fileName">The name of the file.</param>
+    /// <param name="content">
+    /// File content. Can be a composite format string <see cref="string.Format(string, object?[])"/>.
+    /// </param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
     public static void AppendFailureDump(
         this UITestContext context,
         string fileName,
