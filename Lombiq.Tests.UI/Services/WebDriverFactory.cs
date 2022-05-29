67775871bf7f13cec40f52f6604c956cce6b9311
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-29 23:12:35 +0200

    SuppressInitialDiagnosticInformation

--- Lombiq.Tests.UI/Services/WebDriverFactory.cs
+++ Lombiq.Tests.UI/Services/WebDriverFactory.cs
@@ -44,6 +44,7 @@ public static class WebDriverFactory
             configuration.BrowserOptionsConfigurator?.Invoke(chromeConfig.Options);
 
             chromeConfig.Service = service ?? ChromeDriverService.CreateDefaultService();
+            chromeConfig.Service.SuppressInitialDiagnosticInformation = true;
             chromeConfig.Service.WhitelistedIPAddresses += "::ffff:127.0.0.1"; // By default localhost is only allowed in IPv4.
             if (chromeConfig.Service.HostName == "localhost") chromeConfig.Service.HostName = "127.0.0.1"; // Helps with misconfigured hosts.
 
