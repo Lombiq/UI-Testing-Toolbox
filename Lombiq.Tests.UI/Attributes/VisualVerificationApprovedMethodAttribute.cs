ce31d48b7819b3412f6688cc6bdd0664d346caf5
József Horváth <jozsef.horvath@lombiq.com>
2022-06-23 18:32:04 +0200

    adding comments

--- Lombiq.Tests.UI/Attributes/VisualVerificationApprovedMethodAttribute.cs
+++ Lombiq.Tests.UI/Attributes/VisualVerificationApprovedMethodAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace Lombiq.Tests.UI.Attributes;
 
 /// <summary>
-/// This attribute is used to mark VisualVerificationApproved methods in the call stack.
+/// This attribute is used to mark the VisualVerificationApproved methods in the call stack to reach the consumer method.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 public sealed class VisualVerificationApprovedMethodAttribute : Attribute
