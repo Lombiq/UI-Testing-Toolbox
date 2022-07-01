87f8df1d500bbfdade5c804a6e23e431673185c6
József Horváth <jozsef.horvath@lombiq.com>
2022-07-01 18:04:52 +0200

    Comments

--- Lombiq.Tests.UI/Extensions/ElementStyleUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/ElementStyleUITestContextExtensions.cs
@@ -4,10 +4,17 @@ using OpenQA.Selenium;
 namespace Lombiq.Tests.UI.Extensions;
 public static class ElementStyleUITestContextExtensions
 {
-    public static void SetElementStyle(this UITestContext context, By elementSelector, string rule, string value) =>
+    /// <summary>
+    /// Sets the element inline style.
+    /// </summary>
+    /// <param name="context">The <see cref="UITestContext"/> in which the extension is executed on.</param>
+    /// <param name="elementSelector">Selector for the target element.</param>
+    /// <param name="property">CSS property name.</param>
+    /// <param name="value">CSS property value.</param>
+    public static void SetElementStyle(this UITestContext context, By elementSelector, string property, string value) =>
         context.ExecuteScript(
             "arguments[0].style[arguments[1]] = arguments[2];",
             context.Get(elementSelector),
-            rule,
+            property,
             value);
 }
