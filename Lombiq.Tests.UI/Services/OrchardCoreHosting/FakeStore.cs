cc9064b6a9ae9afdddf796304d071c2aa2d38ef9
József Horváth <jozsef.horvath@lombiq.com>
2022-07-18 21:57:31 +0200

    Injecting FakeStore

---
+++ Lombiq.Tests.UI/Services/OrchardCoreHosting/FakeStore.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YesSql;
+using YesSql.Indexes;
+
+namespace Lombiq.Tests.UI.Services.OrchardCoreHosting;
+
+public class FakeStore : IStore
+{
+    private readonly List<ISession> _createdSessions = new();
+    private readonly IStore _store;
+
+    public FakeStore(IStore store) =>
+        _store = store;
+
+    public IConfiguration Configuration => _store.Configuration;
+
+    public ITypeService TypeNames => _store.TypeNames;
+
+    public ISession CreateSession()
+    {
+        lock (_createdSessions)
+        {
+            var session = _store.CreateSession();
+            _createdSessions.Add(session);
+
+            return session;
+        }
+    }
+
+    public IEnumerable<IndexDescriptor> Describe(Type target, string collection = null) => _store.Describe(target, collection);
+
+    public void Dispose()
+    {
+        foreach (var session in _createdSessions)
+        {
+            try
+            {
+                session.Dispose();
+            }
+            catch
+#pragma warning disable S108 // Nested blocks of code should not be left empty
+            {
+            }
+#pragma warning restore S108 // Nested blocks of code should not be left empty
+        }
+
+        _createdSessions.Clear();
+
+        _store?.Dispose();
+
+        GC.Collect();
+    }
+
+    public Task InitializeAsync() => _store.InitializeAsync();
+
+    public Task InitializeCollectionAsync(string collection) => _store.InitializeCollectionAsync(collection);
+
+    public IStore RegisterIndexes(IEnumerable<IIndexProvider> indexProviders, string collection = null) =>
+        _store.RegisterIndexes(indexProviders, collection);
+}
--- Lombiq.Tests.UI/Services/OrchardCoreHosting/OrchardApplicationFactory.cs
+++ Lombiq.Tests.UI/Services/OrchardCoreHosting/OrchardApplicationFactory.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using YesSql;
 
 namespace Lombiq.Tests.UI.Services.OrchardCoreHosting;
 
@@ -11,6 +17,7 @@ public class OrchardApplicationFactory<TStartup> : WebApplicationFactory<TStartu
    where TStartup : class
 {
     private readonly Action<IWebHostBuilder> _configuration;
+    private readonly List<IStore> _createdStores = new();
     private IHost _host;
 
     public OrchardApplicationFactory(string rootUrl, Action<IWebHostBuilder> configuration = null)
@@ -19,7 +26,11 @@ public class OrchardApplicationFactory<TStartup> : WebApplicationFactory<TStartu
         _configuration = configuration;
     }
 
-    protected override void ConfigureWebHost(IWebHostBuilder builder) => _configuration?.Invoke(builder);
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(ConfigureTestServices);
+        _configuration?.Invoke(builder);
+    }
 
     protected override IHost CreateHost(IHostBuilder builder)
     {
@@ -28,12 +39,45 @@ public class OrchardApplicationFactory<TStartup> : WebApplicationFactory<TStartu
         // configure and start the actual host.
         builder.ConfigureWebHost(webHostBuilder =>
             webHostBuilder.UseKestrel()
-                .UseUrls(RootUrl));
+                .UseUrls(RootUrl)
+                .ConfigureTestServices(ConfigureTestServices));
 
         _host = builder.Build();
         _host.Start();
 
         return host;
+    }
+
+    private void ConfigureTestServices(IServiceCollection services)
+    {
+        var builder = services
+                .LastOrDefault(d => d.ServiceType == typeof(OrchardCoreBuilder))?
+                .ImplementationInstance as OrchardCoreBuilder;
+        builder.ConfigureServices(builderServices => AddFakeStore(builderServices));
+    }
+
+    private void AddFakeStore(IServiceCollection services)
+    {
+        var storeDescriptor = services.LastOrDefault(d => d.ServiceType == typeof(IStore));
+
+        services.RemoveAll<IStore>();
+
+        services.AddSingleton<IStore>(serviceProvider =>
+        {
+            var store = (IStore)storeDescriptor.ImplementationFactory.Invoke(serviceProvider);
+            if (store is null)
+            {
+                return null;
+            }
+
+            lock (_createdStores)
+            {
+                var fakeStore = new FakeStore((IStore)storeDescriptor.ImplementationFactory.Invoke(serviceProvider));
+                _createdStores.Add(fakeStore);
+
+                return fakeStore;
+            }
+        });
     }
 
     // WebApplicationFactory<>.DisposeAsync() calls "GC.SuppressFinalize".
@@ -42,10 +86,19 @@ public class OrchardApplicationFactory<TStartup> : WebApplicationFactory<TStartu
 #pragma warning restore CA1816 // Call GC.SuppressFinalize correctly
     {
         await _host?.StopAsync();
+
+        foreach (var store in _createdStores)
+        {
+            store.Dispose();
+        }
+
+        _createdStores.Clear();
+
         _host?.Dispose();
         _host = null;
 
         await base.DisposeAsync();
+        GC.Collect();
     }
 
     public string RootUrl { get; private set; }
