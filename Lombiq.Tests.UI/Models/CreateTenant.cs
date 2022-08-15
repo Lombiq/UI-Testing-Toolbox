6e8deda27b60eaca62c9f2d06d2901d3651f03de
Dávid El-Saig <david.el-saig@lombiq.com>
2022-08-15 16:37:54 +0200

    Change language to just "en".
    No idea wtf is going on here, I have "en-US" locally, but on the server only "en" exists. A mystery for the ages...

--- Lombiq.Tests.UI/Models/CreateTenant.cs
+++ Lombiq.Tests.UI/Models/CreateTenant.cs
@@ -7,7 +7,7 @@ public class CreateTenant
     public string ConnectionString { get; set; } = string.Empty;
     public string DatabaseProvider { get; set; } = "Sqlite";
     public string TimeZone { get; set; }
-    public string Language { get; set; } = "en-US";
+    public string Language { get; set; } = "en";
     public string UserName { get; set; } = DefaultUser.UserName;
     public string Email { get; set; } = DefaultUser.Email;
     public string Password { get; set; } = DefaultUser.Password;
