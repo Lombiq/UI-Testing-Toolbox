6a5f5cf80a7e7c369043d6fffb8baa45cf283002
Roland Márkus <roland.markus@lombiq.com>
2022-10-18 16:00:38 +0200

    Adding logout filter.

--- Lombiq.Tests.UI/MonkeyTesting/MonkeyTestingOptions.cs
+++ Lombiq.Tests.UI/MonkeyTesting/MonkeyTestingOptions.cs
@@ -53,6 +53,9 @@ public sealed class MonkeyTestingOptions
             log: true,
             canClick: (element) => {
                     for (; element && element !== document; element = element.parentNode) {
+                        if (element.getAttribute('action') === '/Users/LogOff') {
+                            return false;
+                        }
                         if (!(!element.hasAttribute('href') || /https:\/\/localhost:\d\d\d\d.*/.test(element.href))) {
                             return false;
                         }
@@ -138,6 +141,12 @@ public sealed class MonkeyTestingOptions
     public MonkeyTestingOptions ExcludeRegisterPageFromMonkeyTesting()
     {
         UrlFilters.Add(new NotStartsWithMonkeyTestingUrlFilter("/Register"));
+        return this;
+    }
+
+    public MonkeyTestingOptions ExcludeLogOffPageFromMonkeyTesting()
+    {
+        UrlFilters.Add(new NotStartsWithMonkeyTestingUrlFilter("/Users/LogOff"));
         return this;
     }
 
