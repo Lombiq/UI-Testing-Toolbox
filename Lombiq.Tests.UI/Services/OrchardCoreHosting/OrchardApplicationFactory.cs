d15ee8bb1eae4ee2816f9b06fb84a180cdc48346
József Horváth <jozsef.horvath@lombiq.com>
2022-12-05 08:20:53 +0100

    Update Lombiq.Tests.UI/Services/OrchardCoreHosting/OrchardApplicationFactory.cs
    Co-authored-by: Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>

--- Lombiq.Tests.UI/Services/OrchardCoreHosting/OrchardApplicationFactory.cs
+++ Lombiq.Tests.UI/Services/OrchardCoreHosting/OrchardApplicationFactory.cs
@@ -47,10 +47,11 @@ public sealed class OrchardApplicationFactory<TStartup> : WebApplicationFactory<
     protected override IHost CreateHost(IHostBuilder builder)
     {
         builder.ConfigureHostConfiguration(configurationBuilder => _configureHost?.Invoke(configurationBuilder));
-        // This lock is to avoid parallel start of the application. The
-        // Microsoft.Extensions.Hosting.HostFactoryResolver.HostingListener.CreateHost() starts a new thread for the
-        // web application instance which can cause issues in eg.: NLog.Config.Factory<TBaseType, TAttributeType>.RegisterDefinition()
-        // which is using not thread safe Dictionary to store cached types when initializing default logger instance.
+        // This lock is to avoid parallel start of the application.
+        // Microsoft.Extensions.Hosting.HostFactoryResolver.HostingListener.CreateHost() starts a new thread for the web
+        // application instance which can cause issues in e.g.:
+        // NLog.Config.Factory<TBaseType, TAttributeType>.RegisterDefinition() which is using non-thread-safe Dictionary
+        // to store cached types when initializing the default logger instance.
         lock (OrchardApplicationFactoryCounter.CreateHostLock)
         {
             return base.CreateHost(builder);
