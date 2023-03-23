9f944b28cba280dea412c1466710c012e52ceee6
József Horváth <jozsef.horvath@lombiq.com>
2023-03-23 15:04:51 +0100

    Adding docs Refactoring

--- Lombiq.Tests.UI/Constants/DirectoryPaths.cs
+++ Lombiq.Tests.UI/Constants/DirectoryPaths.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 
 namespace Lombiq.Tests.UI.Constants;
 
@@ -11,10 +10,13 @@ public static class DirectoryPaths
     public const string Screenshots = nameof(Screenshots);
 
     public static string GetTempDirectoryPath(params string[] subDirectoryNames) =>
-        Path.Combine(new[] { Environment.CurrentDirectory, Temp }.Concat(subDirectoryNames).ToArray());
+        Path.Combine(
+            Path.Combine(Environment.CurrentDirectory, Temp),
+            Path.Combine(subDirectoryNames));
 
     public static string GetTempSubDirectoryPath(string contextId, params string[] subDirectoryNames) =>
-        GetTempDirectoryPath(new[] { contextId }.Concat(subDirectoryNames).ToArray());
+        GetTempDirectoryPath(
+            Path.Combine(contextId, Path.Combine(subDirectoryNames)));
 
     public static string GetScreenshotsDirectoryPath(string contextId) =>
         GetTempSubDirectoryPath(contextId, Screenshots);
