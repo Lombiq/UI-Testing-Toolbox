e211e1c6db86f827e91a7bf418e2bd713473a6b0
Oliver Friedrich <oliver.friedrich@lombiq.com>
2023-04-15 13:49:37 +0200

    Move static fields and read-only properties to beginning of file

--- Lombiq.Tests.UI/Services/OrchardCoreUITestExecutorConfiguration.cs
+++ Lombiq.Tests.UI/Services/OrchardCoreUITestExecutorConfiguration.cs
@@ -21,6 +21,40 @@ public enum Browser
 
 public class OrchardCoreUITestExecutorConfiguration
 {
+    public static readonly Func<IWebApplicationInstance, Task> AssertAppLogsAreEmptyAsync = app => app.LogsShouldBeEmptyAsync();
+
+    public static readonly Func<IWebApplicationInstance, Task> AssertAppLogsCanContainWarningsAsync =
+        app => app.LogsShouldBeEmptyAsync(canContainWarnings: true);
+
+    public static readonly Action<IEnumerable<LogEntry>> AssertBrowserLogIsEmpty =
+        logEntries => logEntries.ShouldNotContain(
+            logEntry => IsValidBrowserLogEntry(logEntry),
+            logEntries.Where(IsValidBrowserLogEntry).ToFormattedString());
+
+    public static readonly Func<LogEntry, bool> IsValidBrowserLogEntry =
+        logEntry =>
+            logEntry.Level >= LogLevel.Warning &&
+            // HTML imports are somehow used by Selenium or something but this deprecation notice is always there for every
+            // page.
+            !logEntry.Message.ContainsOrdinalIgnoreCase("HTML Imports is deprecated") &&
+            // The 404 is because of how browsers automatically request /favicon.ico even if a favicon is declared to be
+            // under a different URL.
+            !logEntry.IsNotFoundLogEntry("/favicon.ico");
+
+    /// <summary>
+    /// Gets the global events available during UI test execution.
+    /// </summary>
+    public UITestExecutionEvents Events { get; } = new();
+
+    /// <summary>
+    /// Gets a dictionary storing some custom configuration data.
+    /// </summary>
+    [SuppressMessage(
+        "Design",
+        "MA0016:Prefer return collection abstraction instead of implementation",
+        Justification = "Deliberately modifiable by consumer code.")]
+    public Dictionary<string, object> CustomConfiguration { get; } = new();
+
     public BrowserConfiguration BrowserConfiguration { get; set; } = new();
     public TimeoutConfiguration TimeoutConfiguration { get; set; } = TimeoutConfiguration.Default;
     public AtataConfiguration AtataConfiguration { get; set; } = new();
@@ -136,20 +170,6 @@ public class OrchardCoreUITestExecutorConfiguration
     /// </summary>
     public ShortcutsConfiguration ShortcutsConfiguration { get; set; } = new();
 
-    /// <summary>
-    /// Gets the global events available during UI test execution.
-    /// </summary>
-    public UITestExecutionEvents Events { get; } = new();
-
-    /// <summary>
-    /// Gets a dictionary storing some custom configuration data.
-    /// </summary>
-    [SuppressMessage(
-        "Design",
-        "MA0016:Prefer return collection abstraction instead of implementation",
-        Justification = "Deliberately modifiable by consumer code.")]
-    public Dictionary<string, object> CustomConfiguration { get; } = new();
-
     public async Task AssertAppLogsMaybeAsync(IWebApplicationInstance instance, Action<string> log)
     {
         if (instance == null || AssertAppLogsAsync == null) return;
@@ -183,24 +203,4 @@ public class OrchardCoreUITestExecutorConfiguration
             throw;
         }
     }
-
-    public static readonly Func<IWebApplicationInstance, Task> AssertAppLogsAreEmptyAsync = app => app.LogsShouldBeEmptyAsync();
-
-    public static readonly Func<IWebApplicationInstance, Task> AssertAppLogsCanContainWarningsAsync =
-        app => app.LogsShouldBeEmptyAsync(canContainWarnings: true);
-
-    public static readonly Action<IEnumerable<LogEntry>> AssertBrowserLogIsEmpty =
-        // HTML imports are somehow used by Selenium or something but this deprecation notice is always there for every
-        // page.
-        logEntries => logEntries.ShouldNotContain(
-            logEntry => IsValidBrowserLogEntry(logEntry),
-            logEntries.Where(IsValidBrowserLogEntry).ToFormattedString());
-
-    public static readonly Func<LogEntry, bool> IsValidBrowserLogEntry =
-        logEntry =>
-            logEntry.Level >= LogLevel.Warning &&
-            !logEntry.Message.ContainsOrdinalIgnoreCase("HTML Imports is deprecated") &&
-            // The 404 is because of how browsers automatically request /favicon.ico even if a favicon is declared to be
-            // under a different URL.
-            !logEntry.IsNotFoundLogEntry("/favicon.ico");
 }
