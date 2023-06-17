32a90974080bbdeb0bcfdbaceccb765473fab32f
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-17 14:50:14 +0200

    Add helpers to create By for content picker and interpolated CSS.

--- Lombiq.Tests.UI/Extensions/NavigationUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/NavigationUITestContextExtensions.cs
@@ -1,6 +1,7 @@
 using Atata;
 using Lombiq.HelpfulLibraries.Common.Utilities;
 using Lombiq.Tests.UI.Constants;
+using Lombiq.Tests.UI.Helpers;
 using Lombiq.Tests.UI.Pages;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
@@ -229,26 +230,25 @@ public static class NavigationUITestContextExtensions
 
     public static Task SetTaxonomyFieldByIndexAsync(this UITestContext context, string taxonomyId, int index)
     {
-        var baseSelector = StringHelper.CreateInvariant($".tags[data-taxonomy-content-item-id='{taxonomyId}']");
+        var baseSelector = ByHelper.Css($".tags[data-taxonomy-content-item-id='{taxonomyId}']");
         return SetFieldDropdownByIndexAsync(context, baseSelector, index);
     }
 
     public static Task SetContentPickerByIndexAsync(this UITestContext context, string part, string field, int index)
     {
-        var baseSelector = StringHelper.CreateInvariant($"*[data-part='{part}'][data-field='{field}']");
+        var baseSelector = ByHelper.GetContentPickerSelector(part, field);
         return SetFieldDropdownByIndexAsync(context, baseSelector, index);
     }
 
-    private static async Task SetFieldDropdownByIndexAsync(UITestContext context, string baseSelector, int index)
+    private static async Task SetFieldDropdownByIndexAsync(UITestContext context, By baseSelector, int index)
     {
-        var byItem =
-            By.CssSelector(StringHelper.CreateInvariant(
-                $"{baseSelector} .multiselect__element:nth-child({index + 1}) .multiselect__option"))
+        var byItem = baseSelector
+            .Then(ByHelper.Css($".multiselect__element:nth-child({index + 1}) .multiselect__option"))
             .Visible();
 
         while (!context.Exists(byItem.Safely()))
         {
-            await context.ClickReliablyOnAsync(By.CssSelector(baseSelector + " .multiselect__select"));
+            await context.ClickReliablyOnAsync(baseSelector.Then(By.CssSelector(".multiselect__select")));
         }
 
         await context.ClickReliablyOnAsync(byItem);
