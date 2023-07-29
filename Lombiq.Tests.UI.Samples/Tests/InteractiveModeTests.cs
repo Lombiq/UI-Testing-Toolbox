8953ad7abd741d8879396ed4a06e1d97f5565071
Dávid El-Saig <david.el-saig@lombiq.com>
2023-07-29 08:53:47 +0200

    Added sample with comments.

--- Lombiq.Tests.UI.Samples/Tests/InteractiveModeTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/InteractiveModeTests.cs
@@ -3,6 +3,7 @@ using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
 using OpenQA.Selenium;
 using Shouldly;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -15,6 +16,28 @@ public class InteractiveModeTests : UITestBase
         : base(testOutputHelper)
     {
     }
+
+    [Theory(Skip = "Use this to test to try out the interactive mode. This is not a real test you can run in CI."), Chrome]
+    [SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "Only a demo.")]
+    public Task SampleTest(Browser browser) =>
+        ExecuteTestAfterSetupAsync(
+            async context =>
+            {
+                await context.SignInDirectlyAsync();
+
+                // One use-case of interactive mode is to look around in the admin dashboard and troubleshoot the
+                // current settings without programmatically navigating there.
+                await context.SignInDirectlyAndGoToDashboardAsync();
+                await context.SwitchToInteractiveAsync();
+
+                // Afterwards if you can still evaluate code as normal so `SignInDirectlyAndGoToDashboardAsync()` can be
+                // inserted anywhere. Bare in mind, that it's safest to use it before code that's already going to
+                // navigate away, like `GetCurrentUserNameAsync()` here. This ensures any manual navigation you did
+                // won't affect the test. If that's not practical, you can also do your thing in a new tab and close it
+                // before continuing the test.
+                (await context.GetCurrentUserNameAsync()).ShouldNotBeNullOrWhiteSpace();
+            },
+            browser);
 
     [Theory, Chrome]
     public Task EnteringInteractiveModeShouldWait(Browser browser) =>
