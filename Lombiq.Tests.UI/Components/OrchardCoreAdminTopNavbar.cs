f220bc6cd820e443bfecadcfe5060852a5a82dec
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-10-16 13:24:47 +0200

    Modifying find attribute to match with new layout of new version of OC

--- Lombiq.Tests.UI/Components/OrchardCoreAdminTopNavbar.cs
+++ Lombiq.Tests.UI/Components/OrchardCoreAdminTopNavbar.cs
@@ -7,10 +7,11 @@ namespace Lombiq.Tests.UI.Components;
 public class OrchardCoreAdminTopNavbar<TOwner> : Control<TOwner>
     where TOwner : PageObject<TOwner>
 {
-    [FindFirst]
+    [FindById("navbarDropdown")]
     public AccountDropdown Account { get; private set; }
 
-    public class AccountDropdown : BSDropdown<TOwner>
+
+    public class AccountDropdown : BSDropdownToggle<TOwner>
     {
         [FindByContent(TermMatch.Contains, TermCase.Sentence)]
         public Button<TOwner> LogOff { get; private set; }
