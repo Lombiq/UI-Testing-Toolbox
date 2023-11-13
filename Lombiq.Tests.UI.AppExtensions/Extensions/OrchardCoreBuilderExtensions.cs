e3e29ce40133d8cb50ecc01041df690308771df2
Sára El-Saig <sara.el-saig@lombiq.com>
2023-11-13 17:06:35 +0100

    Add xmldoc.

--- Lombiq.Tests.UI.AppExtensions/Extensions/OrchardCoreBuilderExtensions.cs
+++ Lombiq.Tests.UI.AppExtensions/Extensions/OrchardCoreBuilderExtensions.cs
@@ -4,6 +4,10 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 public static class OrchardCoreBuilderExtensions
 {
+    /// <summary>
+    /// Enables the <c>OrchardCore.AutoSetup</c> feature if the <paramref name="configuration"/> doesn't indicate UI
+    /// testing.
+    /// </summary>
     public static OrchardCoreBuilder EnableAutoSetupIfNotUITesting(
         this OrchardCoreBuilder orchardCoreBuilder,
         IConfiguration configuration) =>
