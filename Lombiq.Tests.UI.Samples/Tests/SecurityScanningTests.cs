f174c2f0f7631ef99127a90362f5a1a5173f37e7
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-11-14 21:50:02 +0100

    Making SecurityScanShouldPass pass with simplified setup

--- Lombiq.Tests.UI.Samples/Tests/SecurityScanningTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/SecurityScanningTests.cs
@@ -1,5 +1,9 @@
 using Lombiq.Tests.UI.Attributes;
+using Lombiq.Tests.UI.Extensions;
+using Lombiq.Tests.UI.Pages;
 using Lombiq.Tests.UI.Services;
+using OpenQA.Selenium;
+using System;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -18,11 +22,39 @@ public class SecurityScanningTests : UITestBase
         ExecuteTestAfterSetupAsync(
             async context =>
             {
-                //await context.SwitchToInteractiveAsync();
-                //await zapManager.StartInstanceAsync("https://localhost:44335/");
                 await context.ZapManager.RunSecurityScanAsync(context, context.Scope.BaseUri);
             },
             browser);
+
+    // Overriding the default setup so we can have a simpler site, simplifying the security scan for the purpose of this
+    // demo.
+    protected override Task ExecuteTestAfterSetupAsync(
+        Func<UITestContext, Task> testAsync,
+        Browser browser,
+        Func<OrchardCoreUITestExecutorConfiguration, Task> changeConfigurationAsync) =>
+        ExecuteTestAsync(
+            testAsync,
+            browser,
+            async context =>
+            {
+                var homepageUri = await context.GoToSetupPageAndSetupOrchardCoreAsync(
+                    new OrchardCoreSetupParameters(context)
+                    {
+                        SiteName = "Lombiq's OSOCE - UI Testing",
+                        RecipeId = "ComingSoon",
+                        TablePrefix = "OSOCE",
+                        SiteTimeZoneValue = "Europe/Budapest",
+                    });
+
+                context.Exists(By.ClassName("masthead-content"));
+
+                return homepageUri;
+            },
+            async configuration =>
+            {
+                configuration.HtmlValidationConfiguration.RunHtmlValidationAssertionOnAllPageChanges = false;
+                await changeConfigurationAsync(configuration);
+            });
 }
 
 // END OF TRAINING SECTION: Security scanning.
