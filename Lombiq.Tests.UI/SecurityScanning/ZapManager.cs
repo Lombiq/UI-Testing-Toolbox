462d7b13e8dca4d8d703d56333ae2717e968e6c6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-11-30 20:38:19 +0100

    Revert "Cleaning up the ZAP image too after completion"
    This reverts commit 116bd83ffd26fb9ca4785014145d7677858e6e37.
    
    It'd actually delete the image after every single test, making a new pull necessary for every test, which is wasteful. We can't reliably run something only after all tests finished, so we can't do this properly.

--- Lombiq.Tests.UI/SecurityScanning/ZapManager.cs
+++ Lombiq.Tests.UI/SecurityScanning/ZapManager.cs
@@ -183,7 +183,7 @@ public sealed class ZapManager : IAsyncDisposable
         return new SecurityScanResult(reportsDirectoryPath, SarifLog.Load(jsonReports[0]));
     }
 
-    public async ValueTask DisposeAsync()
+    public ValueTask DisposeAsync()
     {
         if (_cancellationTokenSource != null && !_cancellationTokenSource.IsCancellationRequested)
         {
@@ -191,23 +191,7 @@ public sealed class ZapManager : IAsyncDisposable
             _cancellationTokenSource.Dispose();
         }
 
-        try
-        {
-            using var cleanupCancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
-            var cleanupCancellationToken = cleanupCancellationTokenSource.Token;
-            var imagesOutput = await _docker.ExecuteAndGetOutputAsync(
-                cleanupCancellationToken, "images", _zapImage, "--format", "{{.ID}}");
-
-            if (!string.IsNullOrEmpty(imagesOutput))
-            {
-                await _docker.ExecuteAsync(cleanupCancellationToken, "image", "rm", _zapImage);
-            }
-        }
-        catch (Exception ex)
-        {
-            _testOutputHelper.WriteLineTimestampedAndDebug(
-                "Removing the Docker image {0} for ZAP failed with the following exception: {1}", _zapImage, ex);
-        }
+        return ValueTask.CompletedTask;
     }
 
     private async Task EnsureInitializedAsync()
