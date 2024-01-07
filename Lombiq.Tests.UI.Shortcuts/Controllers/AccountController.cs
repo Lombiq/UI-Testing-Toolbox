888c0abd98f998bd429581dd836b8a69c88b30cf
Sára El-Saig <sara.el-saig@lombiq.com>
2024-01-07 23:24:46 +0100

    unusing

--- Lombiq.Tests.UI.Shortcuts/Controllers/AccountController.cs
+++ Lombiq.Tests.UI.Shortcuts/Controllers/AccountController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Users;
-using System;
 using System.Threading.Tasks;
 
 namespace Lombiq.Tests.UI.Shortcuts.Controllers;
