2149e2fbf5ab260baf4b4c74c1fa069182e51539
Sára El-Saig <sara.el-saig@lombiq.com>
2024-01-07 04:13:59 +0100

    Permit some error lines in app logs.

--- Lombiq.Tests.UI/Extensions/WebApplicationInstanceExtensions.cs
+++ Lombiq.Tests.UI/Extensions/WebApplicationInstanceExtensions.cs
@@ -1,6 +1,8 @@
 using Lombiq.Tests.UI.Services;
 using Shouldly;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,19 +14,37 @@ public static class WebApplicationInstanceExtensions
     /// Asserting that the logs should be empty. When they aren't the Shouldly exception will contain the logs'
     /// contents.
     /// </summary>
+    /// <param name="permittedStringFragmentsInErrorLines">
+    /// If not <see langword="null"/> or empty, each line is split and any lines containing <c>|ERROR|</c> will be
+    /// ignored if they don't contain any string from this collection (case-insensitive).
+    /// </param>
     public static async Task LogsShouldBeEmptyAsync(
         this IWebApplicationInstance webApplicationInstance,
         bool canContainWarnings = false,
+        ICollection<string> permittedStringFragmentsInErrorLines = null,
         CancellationToken cancellationToken = default)
     {
         if (cancellationToken == default) cancellationToken = CancellationToken.None;
+        permittedStringFragmentsInErrorLines ??= Array.Empty<string>();
 
         var logOutput = await webApplicationInstance.GetLogOutputAsync(cancellationToken);
 
         if (canContainWarnings)
         {
-            logOutput.ShouldNotContain("|ERROR|");
             logOutput.ShouldNotContain("|FATAL|");
+
+            if (permittedStringFragmentsInErrorLines.Any())
+            {
+                logOutput
+                    .SplitByNewLines()
+                    .Where(line => line.Contains("|ERROR|"))
+                    .Where(line => !permittedStringFragmentsInErrorLines.Any(line.ContainsOrdinalIgnoreCase))
+                    .ShouldBeEmpty();
+            }
+            else
+            {
+                logOutput.ShouldNotContain("|ERROR|");
+            }
         }
         else
         {
