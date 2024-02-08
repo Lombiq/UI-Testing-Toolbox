eb18cdecbeb21f807d6ba62f0465ad59c265bc1b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-02-08 01:36:33 +0100

    Docs

--- Lombiq.Tests.UI.Samples/Tests/RemoteTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/RemoteTests.cs
@@ -8,6 +8,15 @@ using Xunit.Abstractions;
 
 namespace Lombiq.Tests.UI.Samples.Tests;
 
+// We recommend always running UI tests on the latest code of your app as part of your CI workflow. So, if anything got
+// broken by a pull request, it should be readily visible. Such tests are self-contained and should not even need access
+// to the internet.
+
+// However, sometimes in addition to this, you also want to test remote apps available online, like running rudimentary
+// smoke tests on your production app (e.g.: Can people still log in? Are payments still working?). The UI Testing
+// Toolbox also supports this. Check out the example below!
+
+// Note how the test derives from RemoteUITestBase this time, not UITestBase.
 public class RemoteTests : RemoteUITestBase
 {
     public RemoteTests(ITestOutputHelper testOutputHelper)
@@ -15,13 +24,22 @@ public class RemoteTests : RemoteUITestBase
     {
     }
 
+    // The test itself is largely the same as all the local ones, but you need to provide a base URI.
     [Fact]
     public Task ExampleDotComShouldWork() =>
         ExecuteTestAsync(
             new Uri("https://example.com/"),
             context =>
             {
+                // Assertions work as usual. Implicit assertions like HTML validation and accessibility checks work too,
+                // and upon a failing assertion a failure dump is generated as you'd expect it.
                 context.Get(By.CssSelector("h1")).Text.ShouldBe("Example Domain");
                 context.Exists(By.LinkText("More information..."));
-            });
+
+                // Note that due to a remote app not being under our control, some things are not supported. E.g., you
+                // can't access the Orchard Core logs.
+            },
+            configuration => configuration.HtmlValidationConfiguration.RunHtmlValidationAssertionOnAllPageChanges = false);
 }
+
+// END OF TRAINING SECTION: Remote tests.
