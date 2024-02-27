19a1f5bab111d7e6ed17e6473fa629786f08de4a
Sára El-Saig <sara.el-saig@lombiq.com>
2024-02-27 09:11:41 +0100

    Break chained test into individual calls because it's completely opaque for debugging.

--- Lombiq.Tests.UI/Extensions/BasicOrchardFeaturesTestingUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/BasicOrchardFeaturesTestingUITestContextExtensions.cs
@@ -541,22 +541,23 @@ public static class BasicOrchardFeaturesTestingUITestContextExtensions
 
                 context.RefreshCurrentAtataContext();
 
+                featuresPage.SearchForFeature(featureName).IsEnabled.Get(out var originalEnabledState);
+                featuresPage.Features[featureName].CheckBox.Check();
+                featuresPage.BulkActions.Toggle.Click();
+
                 featuresPage
-                    .SearchForFeature(featureName).IsEnabled.Get(out bool originalEnabledState)
-                    .Features[featureName].CheckBox.Check()
-                    .BulkActions.Toggle.Click()
+                    .AggregateAssert(page => page
+                        .ShouldContainSuccessAlertMessage(TermMatch.Contains, featureName)
+                        .AdminMenu.FindMenuItem(featureName).IsPresent.Should.Equal(!originalEnabledState)
+                        .SearchForFeature(featureName).IsEnabled.Should.Equal(!originalEnabledState));
+                featuresPage.Features[featureName].CheckBox.Check();
+                featuresPage.BulkActions.Toggle.Click();
 
-                .AggregateAssert(page => page
-                    .ShouldContainSuccessAlertMessage(TermMatch.Contains, featureName)
-                    .AdminMenu.FindMenuItem(featureName).IsPresent.Should.Equal(!originalEnabledState)
-                    .SearchForFeature(featureName).IsEnabled.Should.Equal(!originalEnabledState))
-                .Features[featureName].CheckBox.Check()
-                .BulkActions.Toggle.Click()
-
-                .AggregateAssert(page => page
-                    .ShouldContainSuccessAlertMessage(TermMatch.Contains, featureName)
-                    .AdminMenu.FindMenuItem(featureName).IsPresent.Should.Equal(originalEnabledState)
-                    .SearchForFeature(featureName).IsEnabled.Should.Equal(originalEnabledState));
+                featuresPage
+                    .AggregateAssert(page => page
+                        .ShouldContainSuccessAlertMessage(TermMatch.Contains, featureName)
+                        .AdminMenu.FindMenuItem(featureName).IsPresent.Should.Equal(originalEnabledState)
+                        .SearchForFeature(featureName).IsEnabled.Should.Equal(originalEnabledState));
             });
 
     public static Task TestMediaOperationsAsync(this UITestContext context) =>
