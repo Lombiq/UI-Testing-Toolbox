25e5b974cd6179ba0494d622fc80ecf8626751a0
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-02 13:52:06 +0200

    Temporarily block SqlServerTests.

--- Lombiq.Tests.UI.Samples/Tests/SqlServerTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/SqlServerTests.cs
@@ -1,6 +1,7 @@
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Samples.Extensions;
 using Lombiq.Tests.UI.Services;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -13,6 +14,7 @@ namespace Lombiq.Tests.UI.Samples.Tests;
 // with it, should there be any incompatibilities). Note that for this, you need an SQL Server instance running; by
 // default, this will be attempted under the default localhost server name. If you're using anything else, check out the
 // settings in SqlServerConfiguration and Docs/Configuration.md, especially if you use Docker.
+[SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "Blocked by the linked issue.")]
 public class SqlServerTests : UITestBase
 {
     public SqlServerTests(ITestOutputHelper testOutputHelper)
@@ -22,14 +24,14 @@ public class SqlServerTests : UITestBase
 
     // Here we have basically two of the same tests as in BasicTests but now we're using SQL Server as the site's
     // database. If they still work and there are no errors in the log then the app works with SQL Server too.
-    [Fact]
+    [Fact(Skip = "https://github.com/OrchardCMS/OrchardCore/issues/15628")]
     public Task AnonymousHomePageShouldExistWithSqlServer() =>
         ExecuteTestAfterSetupAsync(
             context => context.CheckIfAnonymousHomePageExistsAsync(),
             // Note the configuration! We could also set this globally in UITestBase.
             configuration => configuration.UseSqlServer = true);
 
-    [Fact]
+    [Fact(Skip = "https://github.com/OrchardCMS/OrchardCore/issues/15628")]
     public Task TogglingFeaturesShouldWorkWithSqlServer() =>
         ExecuteTestAfterSetupAsync(
             context => context.ExecuteAndAssertTestFeatureToggleAsync(),
