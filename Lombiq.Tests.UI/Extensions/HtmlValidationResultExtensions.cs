701f9017ecc72947a76501ae9df2f76d7d713b33
Aydin Erdas <info@aydinerdas.com>
2024-04-08 15:45:39 +0200

    Add parsed html-validate option
    Added ability to get parsed html-validate output

--- Lombiq.Tests.UI/Extensions/HtmlValidationResultExtensions.cs
+++ Lombiq.Tests.UI/Extensions/HtmlValidationResultExtensions.cs
@@ -1,8 +1,11 @@
 using Atata.HtmlValidation;
+using Lombiq.Tests.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Lombiq.Tests.UI.Extensions;
@@ -21,5 +24,45 @@ public static class HtmlValidationResultExtensions
             .Split(Environment.NewLine + Environment.NewLine + "error:", StringSplitOptions.RemoveEmptyEntries)
             .Select(error =>
                 (error.StartsWith("error:", StringComparison.OrdinalIgnoreCase) ? string.Empty : "error:") + error);
+    }
+
+    /// <summary>
+    /// Gets the parsed errors from the HTML validation result.
+    /// Can only be used if the output formatter is set to JSON.
+    /// </summary>
+    public static async Task<IEnumerable<HtmlValidationError>> GetParsedErrorsAsync(this HtmlValidationResult result)
+    {
+        if (string.IsNullOrEmpty(result.ResultFilePath) || !File.Exists(result.ResultFilePath))
+        {
+            return Enumerable.Empty<HtmlValidationError>();
+        }
+
+        return ParseOutput(await File.ReadAllTextAsync(result.ResultFilePath));
+    }
+
+    private static IEnumerable<HtmlValidationError> ParseOutput(string output)
+    {
+        output = output.Trim();
+        if ((!output.StartsWith('{') || !output.EndsWith('}')) &&
+            (!output.StartsWith('[') || !output.EndsWith(']')))
+        {
+            throw new JsonException("Invalid JSON, make sure to set the OutputFormatter to JSON.");
+        }
+
+        try
+        {
+            var document = JsonDocument.Parse(output);
+            return document.RootElement.EnumerateArray()
+                .SelectMany(element => element.GetProperty("messages").EnumerateArray())
+                .Select(message =>
+                {
+                    var rawMessageText = message.GetRawText();
+                    return JsonSerializer.Deserialize<HtmlValidationError>(rawMessageText);
+                });
+        }
+        catch (JsonException)
+        {
+            throw new JsonException("Unable to parse output, did you set the OutputFormatter to JSON?");
+        }
     }
 }
---
+++ Lombiq.Tests.UI/Models/HtmlValidationError.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Core.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Lombiq.Tests.UI.Models;
+
+public class HtmlValidationError
+{
+    [JsonPropertyName("ruleId")]
+    public string RuleId { get; set; }
+    [JsonPropertyName("severity")]
+    public int Severity { get; set; }
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+    [JsonPropertyName("offset")]
+    public int Offset { get; set; }
+    [JsonPropertyName("line")]
+    public int Line { get; set; }
+    [JsonPropertyName("column")]
+    public int Column { get; set; }
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
+    [JsonPropertyName("selector")]
+    public string Selector { get; set; }
+    [JsonPropertyName("ruleUrl")]
+    public string RuleUrl { get; set; }
+    [JsonPropertyName("context")]
+    public JsonElement Context { get; set; }
+}
