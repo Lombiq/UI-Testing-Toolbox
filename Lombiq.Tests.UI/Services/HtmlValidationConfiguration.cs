a698c1c3a717670282e8c5dcb6825950502c67fe
Aydin Erdas <info@aydinerdas.com>
2024-04-22 15:11:42 +0200

    Update HtmlValidationConfiguration.cs

--- Lombiq.Tests.UI/Services/HtmlValidationConfiguration.cs
+++ Lombiq.Tests.UI/Services/HtmlValidationConfiguration.cs
@@ -4,6 +4,7 @@ using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Helpers;
 using Shouldly;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Lombiq.Tests.UI.Services;
@@ -66,18 +67,21 @@ public class HtmlValidationConfiguration
         EnableOnValidatablePagesHtmlValidationAndAssertionOnPageChangeRule;
 
     public static readonly Func<HtmlValidationResult, Task> AssertHtmlValidationOutputIsEmptyAsync =
-        async validationResult =>
+        validationResult =>
         {
             // Keep supporting cases where output format is not set to JSON.
             if (validationResult.Output.Trim().StartsWith('[') ||
                 validationResult.Output.Trim().StartsWith('{'))
             {
-                validationResult.GetParsedErrors().ShouldBeEmpty();
+                var errors = validationResult.GetParsedErrors();
+                errors.ShouldBeEmpty(string.Join('\n', errors.Select(error => error.Message)));
             }
             else
             {
                 validationResult.Output.ShouldBeEmpty();
             }
+
+            return Task.CompletedTask;
         };
 
     public static readonly Predicate<UITestContext> EnableOnValidatablePagesHtmlValidationAndAssertionOnPageChangeRule =
