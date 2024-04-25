0e584318cc9bda62f7788e224d608b892d93f693
Sára El-Saig <sara.el-saig@lombiq.com>
2024-04-25 19:41:58 +0200

    Revert "Update ValidationMessage."
    This reverts commit bac7736ffb3b7bff2c0f7c4f5ddd79f7cfad300b.

--- Lombiq.Tests.UI/Components/ValidationMessage.cs
+++ Lombiq.Tests.UI/Components/ValidationMessage.cs
@@ -2,7 +2,9 @@ using Atata;
 
 namespace Lombiq.Tests.UI.Components;
 
-[ControlDefinition("div[contains(concat(' ', normalize-space(@class), ' '), ' validation-summary-errors ')]//li")]
+[ControlDefinition(
+    "div[contains(concat(' ', normalize-space(@class), ' '), ' mb-3 ')]" +
+    "//span[contains(concat(' ', normalize-space(@class), ' '), ' field-validation-error ')]")]
 public class ValidationMessage<TOwner> : Text<TOwner>
     where TOwner : PageObject<TOwner>
 {
