67873b9ad6a6c93ffc5a4a8f4516c1ed092aedb8
Szabolcs Deme <szabolcs.deme@lombiq.com>
2024-05-06 23:39:44 +0200

    Adding Audit Trail test

--- Lombiq.Tests.UI/Extensions/BasicOrchardFeaturesTestingUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/BasicOrchardFeaturesTestingUITestContextExtensions.cs
@@ -141,6 +141,7 @@ public static class BasicOrchardFeaturesTestingUITestContextExtensions
         await context.TestContentOperationsAsync(customPageHeaderCheckAsync: customPageHeaderCheckAsync);
         await context.TestTurningFeatureOnAndOffAsync();
         await context.TestMediaOperationsAsync();
+        await context.TestAuditTrailAsync();
         await context.TestLogoutAsync();
     }
 
@@ -168,6 +169,7 @@ public static class BasicOrchardFeaturesTestingUITestContextExtensions
         await context.TestLoginAsync();
         await context.TestContentOperationsAsync(dontCheckFrontend, customPageHeaderCheckAsync: customPageHeaderCheckAsync);
         await context.TestTurningFeatureOnAndOffAsync();
+        await context.TestAuditTrailAsync();
         await context.TestLogoutAsync();
     }
 
@@ -649,6 +651,39 @@ public static class BasicOrchardFeaturesTestingUITestContextExtensions
 
                 context.Missing(By.XPath("//div[text()='Example Folder' and @class='folder-name ms-2']"));
             });
+
+    public static Task TestAuditTrailAsync(this UITestContext context) =>
+    context.ExecuteTestAsync(
+        "Test Audit Trail",
+        async () =>
+        {
+            var auditTrailPath = "/AuditTrail";
+
+            await context.EnableFeatureDirectlyAsync("OrchardCore.AuditTrail");
+            await context.GoToAdminRelativeUrlAsync("/Settings" + auditTrailPath);
+
+            await context.GoToEditorTabAsync("Content");
+
+            await context.SetCheckboxValueAsync(By.XPath("//input[@value='Page']"), isChecked: true);
+
+            await context.ClickReliablyOnSubmitAsync();
+
+            var contentItemsPage = await context.GoToContentItemsPageAsync();
+            context.RefreshCurrentAtataContext();
+            contentItemsPage
+                .CreateNewPage()
+                    .Title.Set("Audit Trail")
+                    .Publish.ClickAndGo()
+                .AlertMessages.Should.Contain(message => message.IsSuccess);
+
+            await context.GoToAdminRelativeUrlAsync(auditTrailPath);
+
+            context.Exists(ByHelper.TextContains("of the Page"));
+
+            context.Exists(ByHelper.TextContains("was published"));
+
+            context.Exists(ByHelper.TextContains("was created"));
+        });
 
     /// <summary>
     /// Executes the <paramref name="testFunctionAsync"/> with the specified <paramref name="testName"/>.
