7769369b71c0a8b664b45a5b34e88fe95650e112
Sára El-Saig <sara.el-saig@lombiq.com>
2024-05-07 22:00:49 +0200

    To ensure we don't encounter any concurrency issue, enable EnableThreadSafetyChecks for all tests.

--- Lombiq.Tests.UI.Shortcuts/Startup.cs
+++ Lombiq.Tests.UI.Shortcuts/Startup.cs
@@ -1,6 +1,7 @@
 using Lombiq.HelpfulLibraries.AspNetCore.Extensions;
 using Lombiq.Tests.UI.Shortcuts.Services;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Data.YesSql;
 using OrchardCore.Modules;
 
 namespace Lombiq.Tests.UI.Shortcuts;
@@ -11,5 +12,8 @@ public class Startup : StartupBase
     {
         services.AddSingleton<IInteractiveModeStatusAccessor, InteractiveModeStatusAccessor>();
         services.AddAsyncResultFilter<ApplicationInfoInjectingFilter>();
+
+        // To ensure we don't encounter any concurrency issue, enable EnableThreadSafetyChecks for all tests.
+        services.Configure<YesSqlOptions>(options => options.EnableThreadSafetyChecks = true);
     }
 }
