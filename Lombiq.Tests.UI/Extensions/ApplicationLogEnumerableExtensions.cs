8393dd8edd7f6afe7d7a3dbb44c1503e6cbcf3f8
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-11-10 21:03:27 +0100

    Not printing the log name for single logs in test outputs

--- Lombiq.Tests.UI/Extensions/ApplicationLogEnumerableExtensions.cs
+++ Lombiq.Tests.UI/Extensions/ApplicationLogEnumerableExtensions.cs
@@ -8,13 +8,25 @@ namespace Lombiq.Tests.UI.Extensions;
 
 public static class ApplicationLogEnumerableExtensions
 {
-    public static async Task<string> ToFormattedStringAsync(this IEnumerable<IApplicationLog> logs) =>
-        string.Join(
+    public static async Task<string> ToFormattedStringAsync(this IEnumerable<IApplicationLog> logs)
+    {
+        var logsArray = logs.ToArray();
+
+        if (logsArray.Length == 1)
+        {
+            return Environment.NewLine + await LogLinesToFormattedStringAsync(logsArray[0]);
+        }
+
+        return string.Join(
             Environment.NewLine + Environment.NewLine,
             await Task.WhenAll(
-                logs.Select(async log =>
+                logsArray.Select(async log =>
                     $"# Log name: {log.Name}" +
                     Environment.NewLine +
                     Environment.NewLine +
-                    string.Join(Environment.NewLine, (await log.GetEntriesAsync()).Select(logEntry => logEntry.ToString())))));
+                    await LogLinesToFormattedStringAsync(log))));
+    }
+
+    private static async Task<string> LogLinesToFormattedStringAsync(IApplicationLog log) =>
+        string.Join(Environment.NewLine, (await log.GetEntriesAsync()).Select(logEntry => logEntry.ToString()));
 }
