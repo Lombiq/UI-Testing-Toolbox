72174f12b682f37cb5d9b662995e4cbf4ca34373
Krisztián Németh <krisztian.nemeth@lombiq.com>
2024-11-12 12:44:14 +0100

    Not needed namespace

--- Lombiq.Tests.UI/Extensions/HttpClientUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/HttpClientUITestContextExtensions.cs
@@ -10,7 +10,6 @@ using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Lombiq.Tests.UI.Extensions;
