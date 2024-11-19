a3a35c7d14e16504d3ef73903b71038b4a7f21a6
Sára El-Saig <sara.el-saig@lombiq.com>
2024-11-19 02:31:02 +0100

    Looks like "ignore-certificate-errors" is needed explicitly.

--- Lombiq.Tests.UI/ui-testing-toolkit.mjs
+++ Lombiq.Tests.UI/ui-testing-toolkit.mjs
@@ -86,7 +86,7 @@ async function runTest(test, configureOptions = null) {
 
     if (browserName !== 'Chrome') throw new Error("Only Chrome is supported at this time");
 
-    let options = new chrome.Options();
+    let options = new chrome.Options().addArguments('ignore-certificate-errors');
 
     const argumentsPath = path.join(tempDirectory, 'BrowserArguments.json');
     if (await _exists(argumentsPath)) {
