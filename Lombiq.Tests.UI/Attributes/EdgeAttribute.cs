68e61ef2ef7aead938f77b6c46093a0c9aa5cf54
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-12-25 03:37:45 +0100

    Fixing analyzer violations

--- Lombiq.Tests.UI/Attributes/EdgeAttribute.cs
+++ Lombiq.Tests.UI/Attributes/EdgeAttribute.cs
@@ -1,7 +1,9 @@
 using Lombiq.Tests.UI.Services;
+using System;
 
 namespace Lombiq.Tests.UI.Attributes;
 
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 public sealed class EdgeAttribute : BrowserAttributeBase
 {
     protected override Browser Browser => Browser.Edge;
--- Lombiq.Tests.UI/Attributes/InternetExplorerAttribute.cs
+++ Lombiq.Tests.UI/Attributes/InternetExplorerAttribute.cs
@@ -1,7 +1,9 @@
 using Lombiq.Tests.UI.Services;
+using System;
 
 namespace Lombiq.Tests.UI.Attributes;
 
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 public sealed class InternetExplorerAttribute : BrowserAttributeBase
 {
     protected override Browser Browser => Browser.InternetExplorer;
