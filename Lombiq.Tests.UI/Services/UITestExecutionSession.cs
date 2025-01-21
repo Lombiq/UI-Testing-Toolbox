ecf04519d843925af699b44775cf5bb2da82c72f
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-01-21 02:14:44 +0100

    Updating debug code

--- Lombiq.Tests.UI/Services/UITestExecutionSession.cs
+++ Lombiq.Tests.UI/Services/UITestExecutionSession.cs
@@ -211,7 +211,7 @@ internal sealed class UITestExecutionSession : IAsyncDisposable
         // handles to the temp folder, that can be cleaned up too.
         if (!string.IsNullOrEmpty(contextId))
         {
-            var funnyFile = Path.Combine(DirectoryPaths.GetTempDirectoryPath(contextId), "Zap1/reports/2025-01-20-ZAP-Report-localhost/console.css");
+            var funnyFile = Path.Combine(DirectoryPaths.GetTempDirectoryPath(contextId), "Zap1/reports/2025-01-21-ZAP-Report-localhost/console.css");
 
             _testOutputHelper.WriteLineTimestampedAndDebug(
                 File.Exists(funnyFile)
