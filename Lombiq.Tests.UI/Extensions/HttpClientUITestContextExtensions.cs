460c75d5a2c010f5467191eece72824ebc3496f6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-01-29 01:09:45 +0100

    Revert "Removing unnecessary suppression"
    This reverts commit e8d5508a4939b4b89fb8953b17dfe43da14f957c.

--- Lombiq.Tests.UI/Extensions/HttpClientUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/HttpClientUITestContextExtensions.cs
@@ -3,6 +3,7 @@ using Lombiq.Tests.UI.Services;
 using Shouldly;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -14,6 +15,10 @@ using System.Threading.Tasks;
 
 namespace Lombiq.Tests.UI.Extensions;
 
+[SuppressMessage(
+        "Reliability",
+        "CA2000:Dispose objects before losing scope",
+        Justification = "Disposed by the HttpClient.")]
 public static class HttpClientUITestContextExtensions
 {
     public static JsonSerializerOptions JsonSerializerOptions { get; } = new(JsonSerializerDefaults.Web);
