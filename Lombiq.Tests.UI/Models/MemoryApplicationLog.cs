70d94c25f8eea54b04100d19f702d97322a3624d
Sára El-Saig <sara.el-saig@lombiq.com>
2025-03-05 16:37:37 +0100

    Add clarification in the comment.

--- Lombiq.Tests.UI/Models/MemoryApplicationLog.cs
+++ Lombiq.Tests.UI/Models/MemoryApplicationLog.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace Lombiq.Tests.UI.Models;
 
 /// <summary>
-/// An <see cref="IApplicationLog"/> implementation where the entries are stored in memory.
+/// An <see cref="IApplicationLog"/> implementation where the entries are stored in memory. This makes the logs easier to inspect in IDE during
+/// debugging and working with a list of this type is easier to filter and aggregate than the base type.
 /// </summary>
 public record MemoryApplicationLog(string Name, IList<IApplicationLogEntry> Entries)
     : IApplicationLog
