57c05f7923b8ad05f50d0aea83121962b7c375bd
József Horváth <jozsef.horvath@lombiq.com>
2025-03-18 01:50:09 +0100

    Implementing IDisposable properly

--- Lombiq.Tests.UI/Services/OrchardCoreHosting/FakeStore.cs
+++ Lombiq.Tests.UI/Services/OrchardCoreHosting/FakeStore.cs
@@ -11,6 +11,7 @@ public sealed class FakeStore : IStore
 {
     private readonly ConcurrentBag<ISession> _createdSessions = [];
     private readonly IStore _store;
+    private bool _isDisposed;
 
     public FakeStore(IStore store) => _store = store;
 
@@ -29,30 +30,43 @@ public sealed class FakeStore : IStore
     public IEnumerable<IndexDescriptor> Describe(Type target, string collection = null) =>
         _store.Describe(target, collection);
 
-    public void Dispose()
-    {
-        foreach (var session in _createdSessions)
-        {
-            try
-            {
-                session.Dispose();
-            }
-            catch
-#pragma warning disable S108 // Nested blocks of code should not be left empty
-            {
-            }
-#pragma warning restore S108 // Nested blocks of code should not be left empty
-        }
-
-        _createdSessions.Clear();
-
-        _store?.Dispose();
-    }
-
     public Task InitializeAsync() => _store.InitializeAsync();
 
     public Task InitializeCollectionAsync(string collection) => _store.InitializeCollectionAsync(collection);
 
     public IStore RegisterIndexes(IEnumerable<IIndexProvider> indexProviders, string collection = null) =>
         _store.RegisterIndexes(indexProviders, collection);
+
+    private void Dispose(bool disposing)
+    {
+        if (!_isDisposed && disposing)
+        {
+            foreach (var session in _createdSessions)
+            {
+                try
+                {
+                    session.Dispose();
+                }
+                catch
+                // The mocked session can cause exception, but we can't do anything with it here.
+#pragma warning disable S108 // Nested blocks of code should not be left empty
+                {
+                }
+#pragma warning restore S108 // Nested blocks of code should not be left empty
+            }
+
+            _createdSessions.Clear();
+
+            _store?.Dispose();
+
+            _isDisposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
 }
