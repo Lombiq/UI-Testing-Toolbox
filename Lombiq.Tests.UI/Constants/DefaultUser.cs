8fef5f86381af8becfec9d60c7f234b9939b5114
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-03-27 00:44:14 +0100

    Proper password for the DefaultUser

--- Lombiq.Tests.UI/Constants/DefaultUser.cs
+++ Lombiq.Tests.UI/Constants/DefaultUser.cs
@@ -3,6 +3,7 @@ namespace Lombiq.Tests.UI.Constants;
 public static class DefaultUser
 {
     public const string UserName = "admin";
-    public const string Email = "admin@admin.com";
-    public const string Password = "Password1!";
+    public const string Email = "admin@example.com";
+    // This is like a real password so browsers don't complain that it's a leaked one.
+    public const string Password = "hAtRoc&WrImeDrAch1s&e=Huy?9ri!lY";
 }
