c07130604b456828291abcdb99c232e8d54d473d
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-03-27 01:05:09 +0100

    Reverting DefaultUser e-mail so it doesn't break ChangeUserSensitiveContentMaintenanceTaskShouldBeExecutedSuccessfully

--- Lombiq.Tests.UI/Constants/DefaultUser.cs
+++ Lombiq.Tests.UI/Constants/DefaultUser.cs
@@ -3,7 +3,7 @@ namespace Lombiq.Tests.UI.Constants;
 public static class DefaultUser
 {
     public const string UserName = "admin";
-    public const string Email = "admin@example.com";
+    public const string Email = "admin@admin.com";
     // This is like a real password so browsers don't complain that it's a leaked one.
     public const string Password = "hAtRoc&WrImeDrAch1s&e=Huy?9ri!lY";
 }
