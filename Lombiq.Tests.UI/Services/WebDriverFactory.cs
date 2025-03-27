dacfe1fea7619e0667a0ae5fcbb3b35ab3b07d88
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-03-27 22:09:34 +0100

    disable-features=OptimizationHints and disable-features=MediaRouter

--- Lombiq.Tests.UI/Services/WebDriverFactory.cs
+++ Lombiq.Tests.UI/Services/WebDriverFactory.cs
@@ -389,14 +389,14 @@ public static class WebDriverFactory
         // Making rendering (more) deterministic. --deterministic-mode is supposed to switch on all of these, but that
         // flag doesn't seem to exist anymore; so switching everything on manually to be safe, but keeping
         // --deterministic-mode too in case it starts working again in the future.
-        options.AddArgument("deterministic-mode");
-        options.AddArgument("run-all-compositor-stages-before-draw");
-        options.AddArgument("disable-new-content-rendering-timeout");
-        options.AddArgument("enable-begin-frame-control");
-        options.AddArgument("disable-threaded-animation");
-        options.AddArgument("disable-threaded-scrolling");
-        options.AddArgument("disable-checker-imaging");
-        options.AddArgument("disable-image-animation-resync");
+        //options.AddArgument("deterministic-mode");
+        //options.AddArgument("run-all-compositor-stages-before-draw");
+        //options.AddArgument("disable-new-content-rendering-timeout");
+        //options.AddArgument("enable-begin-frame-control");
+        //options.AddArgument("disable-threaded-animation");
+        //options.AddArgument("disable-threaded-scrolling");
+        //options.AddArgument("disable-checker-imaging");
+        //options.AddArgument("disable-image-animation-resync");
 
 
 
