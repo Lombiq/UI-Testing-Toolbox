80560b4f9f5dd23b7567751fd7b710f23048a31a
Sára El-Saig <sara.el-saig@lombiq.com>
2025-04-13 20:45:43 +0200

    Always unique index.

--- Lombiq.Tests.UI/Extensions/TestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/TestContextExtensions.cs
@@ -9,11 +9,15 @@ public static class TestContextExtensions
     /// <summary>
     /// Gets a <see langword="string"/> which is safe to use as an Elasticsearch index.
     /// </summary>
+    /// <param name="id">
+    /// A unique identifier that stays the same between setup and test. This ensures that leftover data in the test
+    /// won't be confused with previous runs.
+    /// </param>
     [SuppressMessage(
         "Globalization",
         "CA1308:Normalize strings to uppercase",
         Justification = "Elasticsearch indexes are lowercase only.")]
-    public static string GetElasticserachSafeIndexName(this ITestContext context)
+    public static string GetElasticserachSafeIndexName(this ITestContext context, Guid id)
     {
         var name = context?
             .Test?
@@ -22,10 +26,12 @@ public static class TestContextExtensions
             .RegexReplace("[^a-z0-9]+", "-")
             .Trim('-');
 
-        if (string.IsNullOrEmpty(name)) return Guid.NewGuid().ToString("N");
+        if (string.IsNullOrWhiteSpace(name)) return id.ToString("N");
 
-        return name.Length > 255
-            ? name[..(255 - 32)] + Guid.NewGuid().ToString("N")
-            : name;
+        // An Elasticsearch index can't be longer than 255 character, but that includes the test name, tenant name, GUID
+        // and relative index name. So altogether 100 characters is a reasonable limit for the test name prefix.
+        if (name.Length > 100) name = name[..100];
+
+        return $"{name}-{id:N}";
     }
 }
