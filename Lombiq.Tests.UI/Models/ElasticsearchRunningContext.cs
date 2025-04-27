504da5d9e931cf5a5e69f3d37168832f96ae0a04
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-04-28 00:14:06 +0200

    Nicer Elasticsearch indexing timeout exception

--- Lombiq.Tests.UI/Models/ElasticsearchRunningContext.cs
+++ Lombiq.Tests.UI/Models/ElasticsearchRunningContext.cs
@@ -16,12 +16,14 @@ namespace Lombiq.Tests.UI.Models;
 public record ElasticsearchRunningContext(Guid Id, string Prefix)
 {
     /// <summary>
-    /// Gets the expression that refers to all indexes that starts with <see cref="Prefix"/>. This should only be used
+    /// Gets the expression that refers to all indexes that start with <see cref="Prefix"/>. This should only be used
     /// with <see cref="IElasticClient"/>, because the OrchardCore-specific services automatically apply the prefix from
     /// configuration so it would result in double prefixing.
     /// </summary>
     private IndexName LowLevelIndexName => Indices.Index($"{Prefix}_*");
 
+    // Elasticsearch indexing sometimes takes longer, and the testing starts before it finishes. To prevent that, we are
+    // checking if all of the indexing tasks are finished.
     public Task BeforeTestAsync(UITestContext context) =>
         context.Application.UsingScopeAsync(async provider =>
         {
@@ -37,79 +39,86 @@ public record ElasticsearchRunningContext(Guid Id, string Prefix)
             (await client.Indices.FlushAsync(index, ct: testCancellationToken)).ThrowIfFailed($"flush index \"{index}\"");
             (await client.Indices.RefreshAsync(index, ct: testCancellationToken)).ThrowIfFailed($"refresh index \"{index}\"");
 
-            // Elasticsearch indexing sometimes takes longer, and the testing starts before it finishes. To prevent that,
-            // we are checking if all of the indexing tasks are finished.
-            var elasticIndexManager = provider.GetRequiredService<ElasticIndexManager>();
             var settingsService = provider.GetRequiredService<ElasticIndexSettingsService>();
-            var indexingTaskManager = provider.GetRequiredService<IIndexingTaskManager>();
             var indexSettings = await settingsService.GetSettingsAsync();
             var exactIndexName = indexSettings.FirstOrDefault()?.IndexName;
 
-            const int batchSize = 1000;
-            long lastTaskId = 0;
-            bool hasTask = true;
+            if (exactIndexName == null) return;
 
-            // We are getting the last indexing task (regardless of the state). This function works like a cursor, so
-            // there is no way to get directly the last task in the list. Since we have to give a "count" parameter, we
-            // are retrieving the indexing tasks by batches of 1000. Then if there is no more, we get the last one.
-            if (exactIndexName != null)
+            var timeout = TimeSpan.FromSeconds(60);
+
+            using var timeoutCancellationTokenSource = new CancellationTokenSource(timeout);
+            using var jointCancellationTokenSource = CancellationTokenSource
+                .CreateLinkedTokenSource(testCancellationToken, timeoutCancellationTokenSource.Token);
+            var jointCancellationToken = jointCancellationTokenSource.Token;
+
+            var elasticIndexManager = provider.GetRequiredService<ElasticIndexManager>();
+
+            long? lastFinishedTaskId = null;
+
+            try
             {
-                // We want to set "hasTask" inside the loop.
-#pragma warning disable S1994 // "for" loop increment clauses should modify the loops' counters
-                for (var startIndex = 0; hasTask; startIndex += batchSize)
+                var lastTaskId = await GetLastTaskIdAsync(provider.GetRequiredService<IIndexingTaskManager>());
+
+                // We have the id of the last indexing task that should happen, so we are waiting here for that task to
+                // complete, since "GetLastTaskId()" returns only completed tasks.
+                while (lastFinishedTaskId < lastTaskId || lastFinishedTaskId == null)
                 {
-                    var lastTask = (await indexingTaskManager.GetIndexingTasksAsync(startIndex, batchSize))
-                        .LastOrDefault();
+                    jointCancellationToken.ThrowIfCancellationRequested();
 
-                    hasTask = lastTask != null;
+                    lastFinishedTaskId = await TryGetLastFinishedTaskIdAsync(elasticIndexManager, exactIndexName);
 
-                    if (hasTask)
-                    {
-                        lastTaskId = lastTask.Id;
-                    }
-                }
-#pragma warning restore S1994 // "for" loop increment clauses should modify the loops' counters
-
-                long? lastFinishedTaskId = null;
-
-                var timeout = TimeSpan.FromSeconds(60);
-
-                using var timeoutCancellationTokenSource = new CancellationTokenSource(timeout);
-                using var jointCancellationTokenSource = CancellationTokenSource
-                    .CreateLinkedTokenSource(testCancellationToken, timeoutCancellationTokenSource.Token);
-                var jointCancellationToken = jointCancellationTokenSource.Token;
-
-                try
-                {
-                    // We have the id of the last indexing task that should happen, so we are waiting here for that task to
-                    // complete, since "GetLastTaskId()" returns only completed tasks.
-                    while (lastTaskId > lastFinishedTaskId || lastFinishedTaskId == null)
-                    {
-                        jointCancellationToken.ThrowIfCancellationRequested();
-
-                        lastFinishedTaskId = await TryGetLastTaskIdAsync(elasticIndexManager, exactIndexName);
-
-                        // The indexing takes a couple of seconds, so there is no need to check them so fast: we are adding
-                        // a delay.
-                        await Task.Delay(500, jointCancellationToken);
-                    }
-                }
-                catch (TaskCanceledException ex)
-                {
-                    throw new TaskCanceledException(
-                        "Elasticsearch indexing wasn't finished due to " +
-                            $"{(testCancellationToken.IsCancellationRequested ? "the test being canceled" : $"it not completing within {timeout}")}.",
-                        ex,
-                        jointCancellationToken);
+                    // The indexing takes a couple of seconds, so there is no need to check them so fast: we are adding
+                    // a delay.
+                    await Task.Delay(500, jointCancellationToken);
                 }
             }
+            catch (TaskCanceledException ex)
+            {
+                throw new TaskCanceledException(
+                    "Elasticsearch indexing wasn't finished due to " +
+                        $"{(testCancellationToken.IsCancellationRequested ? "the test being canceled" : $"it not completing within {timeout}")}.",
+                    ex,
+                    jointCancellationToken);
+            }
         });
+
+    public Task AfterTestAsync(UITestContext context) =>
+        context?.Application?.Services is { } ? AfterTestInnerAsync(context) : Task.CompletedTask;
+
+    private static async Task<long> GetLastTaskIdAsync(IIndexingTaskManager indexingTaskManager)
+    {
+        const int batchSize = 1000;
+        long lastTaskId = 0;
+        bool hasTask = true;
+
+        // We are getting the last indexing task (regardless of the state). This function works like a cursor, so
+        // there is no way to get the last task in the list directly. Since we have to provide a "count" parameter,
+        // we are retrieving the indexing tasks by batches of 1000. Then if there are no more, we get the last one.
+        // We want to set "hasTask" inside the loop.
+#pragma warning disable S1994 // "for" loop increment clauses should modify the loops' counters
+        for (var startIndex = 0; hasTask; startIndex += batchSize)
+        {
+            var lastTask = (await indexingTaskManager.GetIndexingTasksAsync(startIndex, batchSize))
+                .LastOrDefault();
+
+            hasTask = lastTask != null;
+
+            if (hasTask)
+            {
+                lastTaskId = lastTask.Id;
+            }
+        }
+#pragma warning restore S1994 // "for" loop increment clauses should modify the loops' counters
+
+        return lastTaskId;
+    }
 
     /// <summary>
     /// Asking for the last task ID can throw an exception if the underlying value is not initialized yet. This method
     /// catches the exception and returns null instead so it can be safely retried.
     /// </summary>
-    private static async Task<long?> TryGetLastTaskIdAsync(ElasticIndexManager elasticIndexManager, string indexName)
+    private static async Task<long?> TryGetLastFinishedTaskIdAsync(ElasticIndexManager elasticIndexManager, string indexName)
     {
         try
         {
@@ -120,9 +129,6 @@ public record ElasticsearchRunningContext(Guid Id, string Prefix)
             return null;
         }
     }
-
-    public Task AfterTestAsync(UITestContext context) =>
-        context?.Application?.Services is { } ? AfterTestInnerAsync(context) : Task.CompletedTask;
 
     private async Task AfterTestInnerAsync(UITestContext context)
     {
