2d66e945bf10961e10faca2fda652b1ea24c5f0f
Krisztián Németh <krisztian.nemeth@lombiq.com>
2025-07-04 15:58:59 +0200

    Add docs

--- Lombiq.Tests.UI/Helpers/TeamsHelper.cs
+++ Lombiq.Tests.UI/Helpers/TeamsHelper.cs
@@ -10,7 +10,20 @@ namespace Lombiq.Tests.UI.Helpers;
 
 public static class TeamsHelper
 {
-    public static Task<bool> SendFailedUiTestTeamsMessageAsync(string webhookUrl)
+    /// <summary>
+    /// Send a message to Teams about failing UI test in production using the <see cref="webhookUrl"/>. Uses the
+    /// <see cref="siteName"/> inside the message to specify which site the failing UI test was for. Includes a link to
+    /// the workflow run that failed.
+    /// <remarks>
+    /// <para>
+    /// This needs a workflow in Teams. Add one starting with the "When a Teams webhook request is received"
+    /// trigger ("Who can trigger the flow?" -> Anyone). Then add the "Post card in a chat or channel" step with
+    /// `@triggerBody()` (the @ is important) as the "Adaptive Card" value. Just paste this into the text box,
+    /// trying to insert it as a dynamic content or expression value won't work.
+    /// </para>
+    /// </remarks>
+    /// </summary>
+    public static Task<HttpResponseMessage> SendFailedUiTestTeamsMessageAsync(string webhookUrl, string siteName)
     {
         // This needs a workflow in Teams. Add one starting with the "When a Teams webhook request is received"
         // trigger ("Who can trigger the flow?" -> Anyone). Then add the "Post card in a chat or channel" step with
@@ -20,14 +33,27 @@ public static class TeamsHelper
             $"https://github.com/{Environment.GetEnvironmentVariable("GITHUB_REPOSITORY")}/actions/runs/" +
             $"{Environment.GetEnvironmentVariable("GITHUB_RUN_ID")}";
 
-        var message = "At least one UI test among the ones that test DotNest Tenants Core Production failed. " +
+        var message = $"At least one UI test among the ones that test {siteName} Production failed. " +
             "This can indicate the production app being down, or a recent change breaking a " +
             "feature in production. Please investigate ASAP.";
 
         return SendTeamsMessageAsync(webhookUrl, "Testing Production failed", message, "Go to workflow run", runUrl);
     }
 
-    public static async Task<bool> SendTeamsMessageAsync(string webhookUrl, string title, string message, string urlText, string url)
+    /// <summary>
+    /// Send a simple message to Teams using the <see cref="webhookUrl"/>. Uses the <see cref="title"/> as the title of
+    /// the message, the <see cref="message"/> as the body, and the <see cref="urlText"/> and <see cref="url"/> as a link
+    /// in the message.
+    /// <remarks>
+    /// <para>
+    /// This needs a workflow in Teams. Add one starting with the "When a Teams webhook request is received"
+    /// trigger ("Who can trigger the flow?" -> Anyone). Then add the "Post card in a chat or channel" step with
+    /// `@triggerBody()` (the @ is important) as the "Adaptive Card" value. Just paste this into the text box,
+    /// trying to insert it as a dynamic content or expression value won't work.
+    /// </para>
+    /// </remarks>
+    /// </summary>
+    public static async Task<HttpResponseMessage> SendTeamsMessageAsync(string webhookUrl, string title, string message, string urlText, string url)
     {
         var adaptiveCardContent = new Dictionary<string, object>
         {
@@ -75,6 +101,6 @@ public static class TeamsHelper
 
         var response = await client.PostAsync(webhookUrl, data, TestContext.Current.CancellationToken);
 
-        return response.IsSuccessStatusCode;
+        return response;
     }
 }
