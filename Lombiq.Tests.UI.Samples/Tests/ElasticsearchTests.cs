4fade766ef732f85217600003edb4aa7cf9fcc22
Sára El-Saig <sara.el-saig@lombiq.com>
2026-06-21 02:03:11 +0200

    Add WalkthroughsShouldWorkCorrectly workaround.

--- Lombiq.Tests.UI.Samples/Tests/ElasticsearchTests.cs
+++ Lombiq.Tests.UI.Samples/Tests/ElasticsearchTests.cs
@@ -1,7 +1,6 @@
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Samples.Helpers;
 using OpenQA.Selenium;
-using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -23,8 +22,7 @@ public class ElasticsearchTests : UITestBase
     {
     }
 
-    [SuppressMessage("Usage", "xUnit1004:Test methods should not be skipped", Justification = "temp")]
-    [Fact(Skip = "Temporarily disabled.")]
+    [Fact]
     public Task ElasticsearchShouldWork() =>
         ExecuteTestAsync(
             async context =>
