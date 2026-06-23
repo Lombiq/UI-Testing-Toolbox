6ca7f23a94abb5f86e743eeefa1bb87da1231c50
Sára El-Saig <sara.el-saig@lombiq.com>
2026-06-23 17:55:14 +0200

    Add more readable feature const.

--- Lombiq.Tests.UI.AdminTheme/Constants/FeatureIds.cs
+++ Lombiq.Tests.UI.AdminTheme/Constants/FeatureIds.cs
@@ -2,5 +2,6 @@ namespace Lombiq.Tests.UI.AdminTheme.Constants;
 
 public static class FeatureIds
 {
-    public const string Area = "Lombiq.Tests.UI.AdminTheme";
+    public const string AdminTheme = "Lombiq.Tests.UI.AdminTheme";
+    public const string Area = AdminTheme;
 }
