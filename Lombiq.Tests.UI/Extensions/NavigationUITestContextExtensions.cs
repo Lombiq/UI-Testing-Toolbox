b694915154477e816d23550c4ccdad3aad51cb25
Sára El-Saig <sara.el-saig@lombiq.com>
2026-07-03 01:47:46 +0200

    Assert logs before failing with "Setup did not succeed." for better info.

--- Lombiq.Tests.UI/Extensions/NavigationUITestContextExtensions.cs
+++ Lombiq.Tests.UI/Extensions/NavigationUITestContextExtensions.cs
@@ -330,7 +330,21 @@ public static class NavigationUITestContextExtensions
             await client.PostAsync(uri, formContent, cancellationToken),
             "script[src*='OrchardCore.Setup']");
 
-        (responseSetupScript == null).ShouldBe(shouldBeSuccess, shouldBeSuccess ? "Setup did not succeed." : "Setup succeeded when it shouldn't.");
+        if (shouldBeSuccess)
+        {
+            if (responseSetupScript != null)
+            {
+                // If setup failed, first try to assert logs so we can return more meaningful errors than just the plain
+                // fact that the setup did not succeed.
+                await context.AssertLogsAsync();
+
+                responseSetupScript.ShouldBeNull("Setup did not succeed.");
+            }
+        }
+        else
+        {
+            responseSetupScript.ShouldNotBeNull("Setup succeeded when it shouldn't.");
+        }
 
         await context.GoToAbsoluteUrlAsync(uri, onlyIfNotAlreadyThere: false);
         return uri;
