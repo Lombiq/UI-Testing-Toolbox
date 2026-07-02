2ebad9e765dbf99f92db89652da272f58caeba65
Zoltan Lehoczky <zoltan.lehoczky@lombiq.com>
2026-07-02 17:48:41 +0200

    Fix analyzer warnings from Meziantou.Analyzer update

--- Lombiq.Tests.UI/Components/ValidationSummaryErrorList.cs
+++ Lombiq.Tests.UI/Components/ValidationSummaryErrorList.cs
@@ -3,6 +3,4 @@ using Atata;
 namespace Lombiq.Tests.UI.Components;
 
 public sealed class ValidationSummaryErrorList<TOwner> : ControlList<ValidationSummaryError<TOwner>, TOwner>
-    where TOwner : PageObject<TOwner>
-{
-}
+    where TOwner : PageObject<TOwner>;
