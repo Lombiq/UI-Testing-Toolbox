e2ce6c8fb702937f281b7ad54aad79a00ba06a40
Zoltan Lehoczky <zoltan.lehoczky@lombiq.com>
2026-08-04 15:52:06 +0200

    Better docs on AxeHtmlReport

--- Lombiq.Tests.UI/Services/AxeHtmlReport.cs
+++ Lombiq.Tests.UI/Services/AxeHtmlReport.cs
@@ -32,8 +32,12 @@ public enum AxeReportTypes
 /// </summary>
 /// <remarks>
 /// <para>
-/// Taken from https://github.com/TroyWalshProf/SeleniumAxeHtmlDotnet and updated. Maybe at one point we can have an
-/// Orchard Core shape template-based, customizable report, if the need is there.
+/// Taken from https://github.com/TroyWalshProf/SeleniumAxeHtmlDotnet and updated. Since that project is unmaintained,
+/// no need to keep the two versions in sync.
+/// </para>
+/// <para>
+/// Maybe at one point we can have an Orchard Core shape template-based, customizable report:
+/// https://github.com/Lombiq/UI-Testing-Toolbox/issues/799.
 /// </para>
 /// <para>
 /// Original license:
@@ -42,26 +46,22 @@ public enum AxeReportTypes
 ///
 /// Copyright (c) 2024 Troy Walsh
 ///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+/// documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+/// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+/// permit persons to whom the Software is furnished to do so, subject to the following conditions:
 ///
-/// The above copyright notice and this permission notice shall be included in all
-/// copies or substantial portions of the Software.
+/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+/// Software.
 ///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/// SOFTWARE.
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+/// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+/// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+/// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 /// </para>
 /// <para>
-/// The legacy <c>TWP.Selenium.Axe.Html</c> API surface is preserved through a compatibility shim.
+/// The legacy <c>TWP.Selenium.Axe.Html</c> API surface is preserved through a compatibility shim, see
+/// <c>Lombiq.Tests.UI/Services/AxeHtmlReportShim.cs</c>.
 /// </para>
 /// </remarks>
 public static class AxeHtmlReport
