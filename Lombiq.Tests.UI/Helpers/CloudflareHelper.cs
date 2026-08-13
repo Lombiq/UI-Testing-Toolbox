e19f0380b2ab6f5317e6d6e818672f1bef8ec315
Zoltan Lehoczky <zoltan.lehoczky@lombiq.com>
2026-08-13 17:32:52 +0200

    Revert "Better fix"
    This reverts commit ab4ce698a1aab87da247565d869c65f9fbf834f7.

--- Lombiq.Tests.UI/Helpers/CloudflareHelper.cs
+++ Lombiq.Tests.UI/Helpers/CloudflareHelper.cs
@@ -69,7 +69,7 @@ internal static class CloudflareHelper
         {
             _cloudflareApi ??= RestService.For<ICloudflareApi>("https://api.cloudflare.com/client/v4", new RefitSettings
             {
-                AuthorizationHeaderValueGetter = (_, _) => ValueTask.FromResult(cloudflareApiToken),
+                AuthorizationHeaderValueGetter = async (_, _) => cloudflareApiToken,
             });
 
             if (!_ipAccessRuleIds.ContainsKey(currentIpRange))
