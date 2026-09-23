747c46d86476c6ba98119d93e2ce34429881f8d3
Sára El-Saig <sara.el-saig@lombiq.com>
2026-09-24 01:33:26 +0200

    Handle ErrorController.HealthCheck if OrchardCore.HealthChecks is not enabled.

--- Lombiq.Tests.UI.Shortcuts/Controllers/ErrorController.cs
+++ Lombiq.Tests.UI.Shortcuts/Controllers/ErrorController.cs
@@ -2,6 +2,7 @@ using Lombiq.HelpfulLibraries.AspNetCore.Mvc;
 using Lombiq.Tests.UI.Shortcuts.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
 
@@ -12,18 +13,18 @@ public sealed class ErrorController : Controller
 {
     public const string ExceptionMessage = "This action intentionally causes an exception!";
 
-    private readonly Lazy<TestHealthCheckStatusAccessor> _testHealthCheckStatusAccessorLazy;
-
-    public ErrorController(Lazy<TestHealthCheckStatusAccessor> testHealthCheckStatusAccessorLazy) =>
-        _testHealthCheckStatusAccessorLazy = testHealthCheckStatusAccessorLazy;
-
     [AllowAnonymous]
     public IActionResult Index() => throw new InvalidOperationException(ExceptionMessage);
 
     [AllowAnonymous]
     public IActionResult HealthCheck(string status, string description)
     {
-        var accessor = _testHealthCheckStatusAccessorLazy.Value;
+        // This service depends on the "OrchardCore.HealthChecks" module, so if it's not registered then we can act like
+        // this action doesn't exist.
+        if (HttpContext.RequestServices.GetService<TestHealthCheckStatusAccessor>() is not { } accessor)
+        {
+            return NotFound();
+        }
 
         accessor.Status = Enum.Parse<HealthStatus>(status ?? nameof(HealthStatus.Unhealthy));
         accessor.Description = description ?? nameof(ErrorController);
